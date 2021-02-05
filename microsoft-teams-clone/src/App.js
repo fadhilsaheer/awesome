a7860e58c91a67e1f1d0376d354de51b368c2af9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-02-05 14:57:08 +0530

    microsoft-teams-clone added chat engine keys

--- microsoft-teams-clone/src/App.js
+++ microsoft-teams-clone/src/App.js
@@ -1,2 +1,15 @@
 import { ChatEngine } from "react-chat-engine"; // react chat engine
-import ".App.css";
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="859923c6-722b-4e31-b0e9-c5fdd5a1612b"
+      userName="ubot"
+      userSecret="123"
+    />
+  );
+};
+
+export default App;
