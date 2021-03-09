d869cb9e336824bcd3a4fde639ee19a06a0b210e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-09 16:59:59 +0530

    added socket in chat.js

--- awesome-js/awesome-app/realtime-chat-app/client/src/components/Chat/Chat.js
+++ awesome-js/awesome-app/realtime-chat-app/client/src/components/Chat/Chat.js
@@ -1,5 +1,40 @@
-const Chat = () => (
-    <h1>Chat</h1>
-);
+import { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import './chat.css';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        let connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", 
+            "timeout" : 10000,                  
+            "transports" : ["websocket"]
+        };
+
+        socket = io(ENDPOINT ,connectionOptions);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit("join", { name, room });
+        
+    }, [ENDPOINT, location.search]);
+
+    return(
+        <div>
+            <h1>Chat</h1>
+        </div>
+    );
+}
  
 export default Chat;
