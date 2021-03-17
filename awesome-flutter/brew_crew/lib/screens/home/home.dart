6d3aff744841ac9db71fcc9258a7ce36b646be58
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-17 08:02:56 +0530

    added logout feature

--- awesome-flutter/brew_crew/lib/screens/home/home.dart
+++ awesome-flutter/brew_crew/lib/screens/home/home.dart
@@ -1,10 +1,27 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("home"),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        title: Text("Brew Crew"),
+        elevation: 0.0,
+        actions: [
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text("Logout"),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
     );
   }
 }
--- awesome-flutter/brew_crew/lib/services/auth.dart
+++ awesome-flutter/brew_crew/lib/services/auth.dart
@@ -37,4 +37,12 @@ class AuthService {
 
   // sign out
 
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
