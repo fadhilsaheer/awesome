a36c737b3c5cefa883a9ffbe2ee1d514a26b7253
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-17 23:42:54 +0530

    created modal and stream for brews

--- awesome-flutter/brew_crew/lib/screens/home/brew_list.dart
+++ awesome-flutter/brew_crew/lib/screens/home/brew_list.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
@@ -10,11 +11,11 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot>(context);
+    final brews = Provider.of<List<Brew>>(context);
 
-    for (var doc in brews.docs) {
-      print(doc.data());
-    }
+    brews.forEach((brew) {
+      print(brew.sugars);
+    });
 
     return Container();
   }
--- awesome-flutter/brew_crew/lib/services/database.dart
+++ awesome-flutter/brew_crew/lib/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
@@ -17,8 +18,20 @@ class DatabaseService {
     });
   }
 
+  // brew list from snapshot
+
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Brew(
+        name: doc.data()['name'] ?? '',
+        strength: doc.data()['strength'] ?? 0,
+        sugars: doc.data()['sugars'] ?? '0',
+      );
+    }).toList();
+  }
+
   // get brew stream
-  Stream<QuerySnapshot> get brews {
-    return brewCollections.snapshots();
+  Stream<List<Brew>> get brews {
+    return brewCollections.snapshots().map(_brewListFromSnapshot);
   }
 }
