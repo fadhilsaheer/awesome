3af4dbf56afce8b5e415bc0ef59101111e93383b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-17 07:01:05 +0530

    created method for signin with anoymous

--- awesome-flutter/brew_crew/lib/services/auth.dart
+++ awesome-flutter/brew_crew/lib/services/auth.dart
@@ -1,2 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final firebase = Firebase.initializeApp();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anonymous
+
+  Future signInAnonymous() async {
+    try {
+      var result = await _auth.signInAnonymously();
+      var user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign in with email and password
+
+  // register email and password
+
+  // sign out
+
+}
