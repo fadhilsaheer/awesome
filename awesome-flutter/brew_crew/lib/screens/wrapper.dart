d96260800a63c57b22689b5ffa2c648561d866dd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-17 07:57:15 +0530

    added check for user

--- awesome-flutter/brew_crew/lib/screens/wrapper.dart
+++ awesome-flutter/brew_crew/lib/screens/wrapper.dart
@@ -1,12 +1,14 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<BrewUser>(context);
     // return either home or authenticate
-    return Authenticate();
+    return user == null ? Authenticate() : Home();
   }
 }
