9045cca04dbc6b4eb741d7a1e1b576261304f75c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-19 06:11:12 +0530

    added hero animation

--- awesome-flutter/ninja_trips/lib/shared/tripList.dart
+++ awesome-flutter/ninja_trips/lib/shared/tripList.dart
@@ -20,7 +20,8 @@ class _TripListState extends State<TripList> {
   void _addTrips() {
     // get data from db
     List<Trip> _trips = [
-      Trip(title: 'Beach Paradise', price: '350', nights: '3', img: 'beach.png'),
+      Trip(
+          title: 'Beach Paradise', price: '350', nights: '3', img: 'beach.png'),
       Trip(title: 'City Break', price: '400', nights: '5', img: 'city.png'),
       Trip(title: 'Ski Adventure', price: '750', nights: '2', img: 'ski.png'),
       Trip(title: 'Space Blast', price: '600', nights: '4', img: 'space.png'),
@@ -34,22 +35,31 @@ class _TripListState extends State<TripList> {
   Widget _buildTile(Trip trip) {
     return ListTile(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => Details(trip: trip)));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => Details(trip: trip)));
       },
       contentPadding: EdgeInsets.all(25),
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text('${trip.nights} nights',
-              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.blue[300])),
-          Text(trip.title, style: TextStyle(fontSize: 20, color: Colors.grey[600])),
+          Text(
+            '${trip.nights} nights',
+            style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue[300]),
+          ),
+          Text(
+            trip.title,
+            style: TextStyle(fontSize: 20, color: Colors.grey[600]),
+          ),
         ],
       ),
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(8.0),
-        child: Image.asset(
-          'images/${trip.img}',
-          height: 50.0,
+        child: Hero(
+          tag: 'location-img-${trip.img}',
+          child: Image.asset('images/${trip.img}', height: 50.0),
         ),
       ),
       trailing: Text('\$${trip.price}'),
@@ -63,7 +73,7 @@ class _TripListState extends State<TripList> {
       itemCount: _tripTiles.length,
       itemBuilder: (context, index) {
         return _tripTiles[index];
-      }
+      },
     );
   }
 }
