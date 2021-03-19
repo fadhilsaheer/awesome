0f36e3ad7e6cddd18dfd4b49d706757d9bbf171a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-19 06:38:15 +0530

    added color animation for heart

--- awesome-flutter/ninja_trips/lib/shared/heart.dart
+++ awesome-flutter/ninja_trips/lib/shared/heart.dart
@@ -5,16 +5,54 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _colorAnimation;
+  bool isFav = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _colorAnimation = ColorTween(
+      begin: Colors.grey[400],
+      end: Colors.red,
+    ).animate(_controller);
+
+    _controller.addStatusListener((status) {
+      setState(() {
+        isFav = status == AnimationStatus.completed ? true : false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: 30,
+          ),
+          onPressed: () {
+            isFav ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
