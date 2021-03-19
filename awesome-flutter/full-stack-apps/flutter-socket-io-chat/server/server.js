5f883360288c7981cdd6c443f107eccd0df08511
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-20 00:21:26 +0530

    simplified server code

--- awesome-flutter/full-stack-apps/flutter-socket-io-chat/server/server.js
+++ awesome-flutter/full-stack-apps/flutter-socket-io-chat/server/server.js
@@ -1,10 +1,6 @@
-const express = require("express");
-const app = express();
-
-const server = require("http").createServer(app);
-
-const socketIo = require("socket.io");
-const io = socketIo(server);
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 io.on("connection", (socket) => {
   console.log("user connected ", socket.id);
