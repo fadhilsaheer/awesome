a656b4d25f2da809f2af57d9850ee8693f677d95
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-19 06:47:44 +0530

    added heart size animation

--- awesome-flutter/ninja_trips/lib/shared/heart.dart
+++ awesome-flutter/ninja_trips/lib/shared/heart.dart
@@ -6,18 +6,32 @@ class Heart extends StatefulWidget {
 }
 
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation _colorAnimation;
   bool isFav = false;
+  AnimationController _controller;
+  Animation<Color> _colorAnimation;
+  Animation<double> _sizeAnimation;
 
   @override
   void initState() {
     super.initState();
 
     _controller = AnimationController(
-      duration: Duration(milliseconds: 200),
+      duration: Duration(milliseconds: 300),
       vsync: this,
     );
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50),
+          weight: 50,
+        ),
+        TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30),
+          weight: 50,
+        ),
+      ],
+    ).animate(_controller);
 
     _colorAnimation = ColorTween(
       begin: Colors.grey[400],
@@ -46,7 +60,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: _colorAnimation.value,
-            size: 30,
+            size: _sizeAnimation.value,
           ),
           onPressed: () {
             isFav ? _controller.reverse() : _controller.forward();
