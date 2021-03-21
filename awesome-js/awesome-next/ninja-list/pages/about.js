608e8d5404ab242fd16826921f0a22fad622dfa9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-21 08:03:24 +0530

    added head tag in about

--- awesome-js/awesome-next/ninja-list/pages/about.js
+++ awesome-js/awesome-next/ninja-list/pages/about.js
@@ -1,10 +1,34 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-      <p>Aute laboris aute aliquip nostrud ea in labore amet sit proident consectetur fugiat reprehenderit exercitation. Adipisicing excepteur et sunt ad fugiat et. Do cupidatat dolor incididunt occaecat eiusmod ea duis officia laborum. Lorem commodo nostrud tempor dolore ex quis. Officia non laborum cupidatat aliquip. Laboris mollit consequat reprehenderit ipsum ipsum excepteur aute. Non proident enim duis Lorem aliquip exercitation sunt irure enim consectetur sunt est proident.</p>
-      <p>Aute laboris aute aliquip nostrud ea in labore amet sit proident consectetur fugiat reprehenderit exercitation. Adipisicing excepteur et sunt ad fugiat et. Do cupidatat dolor incididunt occaecat eiusmod ea duis officia laborum. Lorem commodo nostrud tempor dolore ex quis. Officia non laborum cupidatat aliquip. Laboris mollit consequat reprehenderit ipsum ipsum excepteur aute. Non proident enim duis Lorem aliquip exercitation sunt irure enim consectetur sunt est proident.</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h2>About</h2>
+        <p>
+          Aute laboris aute aliquip nostrud ea in labore amet sit proident
+          consectetur fugiat reprehenderit exercitation. Adipisicing excepteur
+          et sunt ad fugiat et. Do cupidatat dolor incididunt occaecat eiusmod
+          ea duis officia laborum. Lorem commodo nostrud tempor dolore ex quis.
+          Officia non laborum cupidatat aliquip. Laboris mollit consequat
+          reprehenderit ipsum ipsum excepteur aute. Non proident enim duis Lorem
+          aliquip exercitation sunt irure enim consectetur sunt est proident.
+        </p>
+        <p>
+          Aute laboris aute aliquip nostrud ea in labore amet sit proident
+          consectetur fugiat reprehenderit exercitation. Adipisicing excepteur
+          et sunt ad fugiat et. Do cupidatat dolor incididunt occaecat eiusmod
+          ea duis officia laborum. Lorem commodo nostrud tempor dolore ex quis.
+          Officia non laborum cupidatat aliquip. Laboris mollit consequat
+          reprehenderit ipsum ipsum excepteur aute. Non proident enim duis Lorem
+          aliquip exercitation sunt irure enim consectetur sunt est proident.
+        </p>
+      </div>
+    </>
   );
 };
 
