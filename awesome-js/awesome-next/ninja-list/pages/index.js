2c1af04f63a9a586276f20954fc44a4035b5f095
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-21 07:41:34 +0530

    added home styles

--- awesome-js/awesome-next/ninja-list/pages/index.js
+++ awesome-js/awesome-next/ninja-list/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import Link from "next/link";
 
 export default function Home() {
