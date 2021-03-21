0a6568eabedcad4c7e3a0151e62e0f057db32fbf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-21 07:37:33 +0530

    added some content in about page

--- awesome-js/awesome-next/ninja-list/pages/about.js
+++ awesome-js/awesome-next/ninja-list/pages/about.js
@@ -1,5 +1,11 @@
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Aute laboris aute aliquip nostrud ea in labore amet sit proident consectetur fugiat reprehenderit exercitation. Adipisicing excepteur et sunt ad fugiat et. Do cupidatat dolor incididunt occaecat eiusmod ea duis officia laborum. Lorem commodo nostrud tempor dolore ex quis. Officia non laborum cupidatat aliquip. Laboris mollit consequat reprehenderit ipsum ipsum excepteur aute. Non proident enim duis Lorem aliquip exercitation sunt irure enim consectetur sunt est proident.</p>
+      <p>Aute laboris aute aliquip nostrud ea in labore amet sit proident consectetur fugiat reprehenderit exercitation. Adipisicing excepteur et sunt ad fugiat et. Do cupidatat dolor incididunt occaecat eiusmod ea duis officia laborum. Lorem commodo nostrud tempor dolore ex quis. Officia non laborum cupidatat aliquip. Laboris mollit consequat reprehenderit ipsum ipsum excepteur aute. Non proident enim duis Lorem aliquip exercitation sunt irure enim consectetur sunt est proident.</p>
+    </div>
+  );
 };
 
 export default About;
