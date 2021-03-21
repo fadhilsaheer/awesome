43f2a5248eab57577e71883fc7f23679461fffe2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-21 07:59:30 +0530

    added logo for navbar

--- awesome-js/awesome-next/ninja-list/components/Navbar.js
+++ awesome-js/awesome-next/ninja-list/components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src="/logo.png" width={128} height={77} />
       </div>
       <Link href="/">
         <a>Home</a>
