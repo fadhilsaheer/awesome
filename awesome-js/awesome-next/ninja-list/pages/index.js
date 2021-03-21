5b3eaa2aab3ac62372ce46ae4e5b76e168a54e14
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-21 07:43:14 +0530

    applied home styles

--- awesome-js/awesome-next/ninja-list/pages/index.js
+++ awesome-js/awesome-next/ninja-list/pages/index.js
@@ -5,20 +5,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Velit nulla est et aute velit anim est nisi ullamco commodo deserunt
         esse dolor incididunt. Adipisicing proident eiusmod id qui minim. Magna
         cupidatat quis non ad. Ullamco culpa eu esse amet et cupidatat laboris
         sit veniam.
       </p>
-      <p>
+      <p className={styles.text}>
         Velit nulla est et aute velit anim est nisi ullamco commodo deserunt
         esse dolor incididunt. Adipisicing proident eiusmod id qui minim. Magna
         cupidatat quis non ad. Ullamco culpa eu esse amet et cupidatat laboris
         sit veniam.
       </p>
-      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
+      <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
     </div>
   );
 }
