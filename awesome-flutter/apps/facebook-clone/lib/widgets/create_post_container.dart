7daa23cb38dc4bf99afdd29ab3985463c23fed60
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 19:40:49 +0530

    completed create post container

--- awesome-flutter/apps/facebook-clone/lib/widgets/create_post_container.dart
+++ awesome-flutter/apps/facebook-clone/lib/widgets/create_post_container.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 
@@ -9,8 +10,69 @@ class CreatePostContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100.0,
-      color: Colors.orange,
+      padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                backgroundColor: Colors.grey[200],
+                backgroundImage: CachedNetworkImageProvider(
+                  curretUser.imageUrl,
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: 'Whats\'s on your mind?',
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Divider(
+            height: 10.0,
+            thickness: 0.5,
+          ),
+          Container(
+            height: 40.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FlatButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.videocam,
+                    color: Colors.red,
+                  ),
+                  label: Text("Live"),
+                ),
+                VerticalDivider(width: 8.0),
+                FlatButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.photo_library,
+                    color: Colors.green,
+                  ),
+                  label: Text("Photo"),
+                ),
+                VerticalDivider(width: 8.0),
+                FlatButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.video_call,
+                    color: Colors.purpleAccent,
+                  ),
+                  label: Text("Room"),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
