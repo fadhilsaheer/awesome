70443c2550fba7b6d39e1e36683de7c34ef8c6bb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 21:16:30 +0530

    created nav screen

---
+++ awesome-flutter/apps/facebook-clone/lib/screens/nav_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavScreen extends StatefulWidget {
+  @override
+  _NavScreenState createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
