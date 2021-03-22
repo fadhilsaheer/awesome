5a1cfa39e869d2c21acb2475904babcdfc672dd0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 20:10:31 +0530

    added online dot

--- awesome-flutter/apps/facebook-clone/lib/widgets/profile_avatar.dart
+++ awesome-flutter/apps/facebook-clone/lib/widgets/profile_avatar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 
 class ProfileAvatar extends StatelessWidget {
   final String imageUrl;
@@ -12,12 +13,34 @@ class ProfileAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: 20.0,
-      backgroundColor: Colors.grey[200],
-      backgroundImage: CachedNetworkImageProvider(
-        imageUrl,
-      ),
+    return Stack(
+      children: [
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: Colors.grey[200],
+          backgroundImage: CachedNetworkImageProvider(
+            imageUrl,
+          ),
+        ),
+        isActive
+            ? Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  height: 15.0,
+                  width: 15.0,
+                  decoration: BoxDecoration(
+                    color: Palette.online,
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 2.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            : SizedBox.shrink()
+      ],
     );
   }
 }
