61e15bce8a6f6673b9c720204405c4be65a7b865
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 20:36:46 +0530

    completed story widget

--- awesome-flutter/apps/facebook-clone/lib/widgets/profile_avatar.dart
+++ awesome-flutter/apps/facebook-clone/lib/widgets/profile_avatar.dart
@@ -5,10 +5,12 @@ import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 class ProfileAvatar extends StatelessWidget {
   final String imageUrl;
   final bool isActive;
+  final bool hasBorder;
 
   ProfileAvatar({
     @required this.imageUrl,
     this.isActive = false,
+    this.hasBorder = false,
   });
 
   @override
@@ -17,9 +19,11 @@ class ProfileAvatar extends StatelessWidget {
       children: [
         CircleAvatar(
           radius: 20.0,
-          backgroundColor: Colors.grey[200],
-          backgroundImage: CachedNetworkImageProvider(
-            imageUrl,
+          backgroundColor: Palette.facebookBlue,
+          child: CircleAvatar(
+            radius: hasBorder ? 17.0 : 20.0,
+            backgroundColor: Colors.grey[200],
+            backgroundImage: CachedNetworkImageProvider(imageUrl),
           ),
         ),
         isActive
--- awesome-flutter/apps/facebook-clone/lib/widgets/stories.dart
+++ awesome-flutter/apps/facebook-clone/lib/widgets/stories.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
 
 class Stories extends StatelessWidget {
   final User currentUser;
@@ -15,8 +18,9 @@ class Stories extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
       height: 200.0,
-      color: Colors.purple,
+      color: Colors.white,
       child: ListView.builder(
+        scrollDirection: Axis.horizontal,
         itemCount: 1 + stories.length,
         itemBuilder: (BuildContext context, int index) {
           if (index == 0) {
@@ -54,6 +58,64 @@ class _StoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: CachedNetworkImage(
+            imageUrl: isAddStory ? currentUser.imageUrl : story.imageUrl,
+            height: double.infinity,
+            width: 110.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          height: double.infinity,
+          width: 110.0,
+          decoration: BoxDecoration(
+            gradient: Palette.storyGradient,
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        Positioned(
+          top: 8.0,
+          left: 8.0,
+          child: isAddStory
+              ? Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: const Icon(Icons.add),
+                    iconSize: 30.0,
+                    color: Palette.facebookBlue,
+                    onPressed: () {},
+                  ),
+                )
+              : ProfileAvatar(
+                  imageUrl: story.user.imageUrl,
+                  hasBorder: story.isViewed,
+                ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          right: 8.0,
+          child: Text(
+            isAddStory ? 'Add to story' : story.user.name,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        )
+      ],
+    );
   }
 }
