bac4fd933a69851c8b83a2b21f39aca3615d1c3c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 18:17:34 +0530

    added sliver app bar

--- awesome-flutter/apps/facebook-clone/lib/screens/home_screen.dart
+++ awesome-flutter/apps/facebook-clone/lib/screens/home_screen.dart
@@ -1,8 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../config/palette.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              "facebook",
+              style: TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              Container(
+                margin: const EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.search),
+                  iconSize: 30.0,
+                  color: Colors.black,
+                  onPressed: () {},
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
