416e7305a37266136865858d95075e811ab5a83f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 17:55:40 +0530

    changed name in data

--- awesome-flutter/apps/facebook-clone/lib/data/data.dart
+++ awesome-flutter/apps/facebook-clone/lib/data/data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 
 final User currentUser = User(
-  name: 'Marcus Ng',
+  name: 'Fadhil M',
   imageUrl:
       'https://images.unsplash.com/photo-1578133671540-edad0b3d689e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
 );
