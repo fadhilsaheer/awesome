0b9175a0dd995d416f7996785eafa10e149999d6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-22 20:01:01 +0530

    added create room button

---
+++ awesome-flutter/apps/facebook-clone/lib/widgets/profile_avatar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProfileAvatar extends StatelessWidget {
+  final String imageUrl;
+  final bool isActive;
+
+  ProfileAvatar({
+    @required this.imageUrl,
+    this.isActive,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- awesome-flutter/apps/facebook-clone/lib/widgets/rooms.dart
+++ awesome-flutter/apps/facebook-clone/lib/widgets/rooms.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 
 class Room extends StatelessWidget {
@@ -10,7 +11,55 @@ class Room extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 60.0,
-      color: Colors.orange,
+      color: Colors.white,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + onlineUsers.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: _createRoomButton(),
+            );
+          }
+          return Container(
+            margin: const EdgeInsets.all(2.0),
+            height: 20.0,
+            width: 20.0,
+            color: Colors.red,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _createRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      color: Colors.white,
+      borderSide: BorderSide(width: 3.0, color: Colors.blueAccent[100]),
+      textColor: Palette.facebookBlue,
+      child: Row(
+        children: [
+          ShaderMask(
+            shaderCallback: (rect) => Palette.createRoomGradient.createShader(
+              rect,
+            ),
+            child: Icon(
+              Icons.video_call,
+              size: 35.0,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(width: 4.0),
+          Text('Create\nRoom')
+        ],
+      ),
     );
   }
 }
