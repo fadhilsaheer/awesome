c4e65abc85677846b538b7e6a2685489def8969b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-25 15:49:50 +0530

    completed server

--- awesome-python/gui-chat/server.py
+++ awesome-python/gui-chat/server.py
@@ -16,6 +16,19 @@ def broadcast(message):
     for client in clients:
         client.send(message)
 
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
 
 def recieve():
     while True:
@@ -33,7 +46,8 @@ def recieve():
         broadcast(f"{nickname} connected to the server!\n".encode("utf-8"))
         client.send("Connected to the server".encode('utf-8'))
 
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
 
-
-
-
+print("server running")
+recieve()
