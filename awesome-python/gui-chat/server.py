bf7871434f70a47e81b5aa0f5112b0e2d6fab76b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-25 15:43:25 +0530

    added recv function

--- awesome-python/gui-chat/server.py
+++ awesome-python/gui-chat/server.py
@@ -17,3 +17,23 @@ def broadcast(message):
         client.send(message)
 
 
+def recieve():
+    while True:
+        client, address = server.accept()
+        print(f"connected with {str(address)}")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of client is {nickname}")
+
+        broadcast(f"{nickname} connected to the server!\n".encode("utf-8"))
+        client.send("Connected to the server".encode('utf-8'))
+
+
+
+
+
