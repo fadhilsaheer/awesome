5932b41ff91b99ad7c0e6ad3ca3b02a4765e37ba
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-25 16:20:50 +0530

    completed client

--- awesome-python/gui-chat/client.py
+++ awesome-python/gui-chat/client.py
@@ -58,7 +58,10 @@ class Client:
         self.win.mainloop()
 
     def write(self):
-        pass
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+
+        self.input_area.delete('1.0', 'end')
 
     def stop(self):
         self.running = False
@@ -67,5 +70,26 @@ class Client:
         exit(0)
 
     def receive(self):
-        pass
+        while self.running:
+            try:
+                message = self.sock.recv(1024)
 
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode("utf-8"))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state="normal")
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state="disabled")
+
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
+
+
+
+client = Client(HOST, PORT)
