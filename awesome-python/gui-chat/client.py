92be8b89bb453160126da5b5cdc17180194b0a4a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-25 16:10:42 +0530

    added gui code

--- awesome-python/gui-chat/client.py
+++ awesome-python/gui-chat/client.py
@@ -29,9 +29,40 @@ class Client:
         receive_thread.start()
 
     def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg="lightgray")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state="disabled")
+
+        self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+    def write(self):
+        pass
+
+    def stop(self):
         pass
 
     def receive(self):
         pass
 
-    
