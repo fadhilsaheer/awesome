226c6505aeb40c2544c2971c0151f650fb1b70b5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-27 16:38:20 +0530

    fetched selected video from state

--- awesome-flutter/ui-clones/youtube-clone/lib/screens/video_screen.dart
+++ awesome-flutter/ui-clones/youtube-clone/lib/screens/video_screen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_youtube_ui/screens/nav_screen.dart';
 
 class VideoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        child: CustomScrollView(
+          shrinkWrap: true,
+          slivers: [
+            SliverToBoxAdapter(
+              child: Consumer(builder: (context, watch, _) {
+                final selectedVideo = watch(selectVideoProvider).state;
+                return Column();
+              }),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
