77bfa995a3d26b2b4e066ac6e07b0431a64802a2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-27 16:19:02 +0530

    half way on miniplayer

--- awesome-flutter/ui-clones/youtube-clone/lib/screens/nav_screen.dart
+++ awesome-flutter/ui-clones/youtube-clone/lib/screens/nav_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_youtube_ui/data.dart';
 import 'package:flutter_youtube_ui/screens/home.dart';
+import 'package:miniplayer/miniplayer.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 final selectVideoProvider = StateProvider<Video?>((ref) => null);
 
@@ -11,6 +13,8 @@ class NavScreen extends StatefulWidget {
 }
 
 class _NavScreenState extends State<NavScreen> {
+  static const double _playerMinHeight = 60.0;
+
   int _selectedIndex = 0;
 
   final _screens = [
@@ -37,7 +41,81 @@ class _NavScreenState extends State<NavScreen> {
                       offstage: _selectedIndex != i,
                     )))
                 .values
-                .toList(),
+                .toList()
+                  ..add(
+                    Offstage(
+                      offstage: selectedVideo == null,
+                      child: Miniplayer(
+                        maxHeight: MediaQuery.of(context).size.height,
+                        minHeight: _playerMinHeight,
+                        builder: (height, percentage) {
+                          if (selectedVideo == null)
+                            return const SizedBox.shrink();
+                          return Container(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.network(
+                                      selectedVideo.thumbnailUrl,
+                                      height: _playerMinHeight - 4.0,
+                                      width: 120.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Flexible(
+                                              child: Text(
+                                                selectedVideo.title,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .caption!
+                                                    .copyWith(
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                      color: Colors.white,
+                                                    ),
+                                              ),
+                                            ),
+                                            Flexible(
+                                              child: Text(
+                                                selectedVideo.author.username,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .caption!
+                                                    .copyWith(
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                    ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const LinearProgressIndicator(
+                                  value: 0.4,
+                                  valueColor:
+                                      AlwaysStoppedAnimation<Color>(Colors.red),
+                                )
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
           );
         },
       ),
