36b9c81d10743d770bd0879480d455a392cb6972
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-27 19:00:32 +0530

    video info

---
+++ awesome-flutter/ui-clones/youtube-clone/lib/widgets/video_info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_youtube_ui/data.dart';
+
+class VideoInfo extends StatelessWidget {
+  final Video video;
+
+  const VideoInfo({required this.video});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
