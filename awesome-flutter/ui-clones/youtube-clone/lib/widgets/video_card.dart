9e4e901cceef3861f7a9dcf4ca8aae2b1c72d5f5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-27 07:31:27 +0530

    completed video container

--- awesome-flutter/ui-clones/youtube-clone/lib/widgets/video_card.dart
+++ awesome-flutter/ui-clones/youtube-clone/lib/widgets/video_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_youtube_ui/data.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class VideoCard extends StatelessWidget {
   final Video video;
@@ -37,7 +38,57 @@ class VideoCard extends StatelessWidget {
               ),
             )
           ],
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: CircleAvatar(
+                  foregroundImage: NetworkImage(video.author.profileImageUrl),
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        video.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .copyWith(fontSize: 15.0),
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        '${video.author.username} • ${video.viewCount} • ${timeago.format(video.timestamp)}',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context)
+                            .textTheme
+                            .caption!
+                            .copyWith(fontSize: 14 + .0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              GestureDetector(
+                child: Icon(Icons.more_vert, size: 20.0),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
