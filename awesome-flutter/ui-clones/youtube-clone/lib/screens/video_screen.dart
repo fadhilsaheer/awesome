57db5c280812d51207d4efee566ac769e13d2aa1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-27 18:57:11 +0530

    thumbnail and icon button

--- awesome-flutter/ui-clones/youtube-clone/lib/screens/video_screen.dart
+++ awesome-flutter/ui-clones/youtube-clone/lib/screens/video_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_youtube_ui/screens/nav_screen.dart';
+import 'package:miniplayer/miniplayer.dart';
 
 class VideoScreen extends StatelessWidget {
   @override
@@ -14,7 +15,31 @@ class VideoScreen extends StatelessWidget {
             SliverToBoxAdapter(
               child: Consumer(builder: (context, watch, _) {
                 final selectedVideo = watch(selectVideoProvider).state;
-                return Column();
+                return SafeArea(
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Image.network(
+                            selectedVideo!.thumbnailUrl,
+                            height: 220.0,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                          IconButton(
+                              icon: const Icon(Icons.keyboard_arrow_down),
+                              iconSize: 30.0,
+                              onPressed: () {
+                                context
+                                    .read(miniPlayerControllerProvider)
+                                    .state
+                                    .animateToHeight(state: PanelState.MIN);
+                              })
+                        ],
+                      )
+                    ],
+                  ),
+                );
               }),
             )
           ],
