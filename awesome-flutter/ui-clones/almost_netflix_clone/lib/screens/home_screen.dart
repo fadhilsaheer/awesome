7fec707b4c7a829374d22b73adb1b058ca74a58e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-28 07:19:43 +0530

    labels

--- awesome-flutter/ui-clones/almost_netflix_clone/lib/screens/home_screen.dart
+++ awesome-flutter/ui-clones/almost_netflix_clone/lib/screens/home_screen.dart
@@ -123,6 +123,49 @@ class _HomescreenState extends State<Homescreen> {
               },
             ),
           ),
+          Container(
+            height: 90.0,
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: labels.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  width: 160.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFFD45253),
+                        Color(0xFF9E1F28),
+                      ],
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF9E1F28),
+                        offset: Offset(0.0, 2.0),
+                        blurRadius: 6.0,
+                      )
+                    ],
+                  ),
+                  child: Center(
+                    child: Text(
+                      labels[index].toUpperCase(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.8,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
