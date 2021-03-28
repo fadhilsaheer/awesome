a3671e52dca70a375905503b444843e746a1b6ba
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-28 07:08:54 +0530

    movie list

--- awesome-flutter/ui-clones/almost_netflix_clone/lib/screens/home_screen.dart
+++ awesome-flutter/ui-clones/almost_netflix_clone/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:almost_netflix_clone/models/movie_model.dart';
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
@@ -6,6 +7,65 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.8);
+  }
+
+  _movieSelector(int index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3) + 0.6).clamp(0.0, 1.0);
+        }
+
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 270.0,
+            width: Curves.easeInOut.transform(value) * 400.0,
+            child: widget,
+          ),
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black54,
+                  offset: Offset(0.0, 4.0),
+                  blurRadius: 10.0,
+                )
+              ],
+            ),
+            child: Center(
+              child: Hero(
+                tag: movies[index].imageUrl,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image(
+                    image: AssetImage(movies[index].imageUrl),
+                    height: 220.0,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +93,21 @@ class _HomescreenState extends State<Homescreen> {
             iconSize: 30.0,
             color: Colors.black,
           ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 280.0,
+            width: double.infinity,
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: movies.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _movieSelector(index);
+              },
+            ),
+          )
         ],
       ),
     );
