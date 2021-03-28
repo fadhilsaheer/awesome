f8d07a7a53cbad11a65d68865cf0e302285eb773
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-28 06:56:30 +0530

    app bar

---
+++ awesome-flutter/ui-clones/almost_netflix_clone/lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Image(
+          image: AssetImage('assets/images/netflix_logo.png'),
+          width: 120.0,
+        ),
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 30.0),
+          onPressed: () => print('Menu'),
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.black,
+        ),
+        actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.only(right: 30.0),
+            onPressed: () => print('Search'),
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
