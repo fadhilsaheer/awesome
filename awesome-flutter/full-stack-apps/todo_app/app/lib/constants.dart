c59293fa4b752b8c8f5a0b486521381b917d3485
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-28 09:28:41 +0530

    constant file

---
+++ awesome-flutter/full-stack-apps/todo_app/app/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+const String serverUrl = 'http://127.0.0.1:5000';
+
+// colors
+
+const Color primaryColor = Color(0xFF252A36);
+const Color secondayrColor = Color(0xFF333848);
+const Color appGreen = Color(0xFF1ABC9C);
+const Color appWhite = Color(0xFFFFFDFD);
+const Color appRed = Color(0xFFF55757);
