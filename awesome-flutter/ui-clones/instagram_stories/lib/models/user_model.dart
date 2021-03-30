23310e56022a60a0f975688ea0d7213a9eeaeb4e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-30 19:22:32 +0530

    created user model

---
+++ awesome-flutter/ui-clones/instagram_stories/lib/models/user_model.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+class User {
+  final String name;
+  final String profileImageUrl;
+
+  User({
+    @required this.name,
+    @required this.profileImageUrl,
+  });
+}
