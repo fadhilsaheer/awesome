80b635486e7ff545bc19b8b6d2e35d0b8762977f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 15:54:01 +0530

    foreign key

--- awesome-python/flask/flask_web_tuto/website/models.py
+++ awesome-python/flask/flask_web_tuto/website/models.py
@@ -3,10 +3,11 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Notes(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
@@ -14,3 +15,4 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     name = db.Column(db.String(255))
+    notes = db.relationship("Note")
