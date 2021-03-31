42825d300b99685ca8677a1dfbea10738d623a13
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 16:30:42 +0530

    logout

--- awesome-python/flask/flask_web_tuto/website/auth.py
+++ awesome-python/flask/flask_web_tuto/website/auth.py
@@ -30,8 +30,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
