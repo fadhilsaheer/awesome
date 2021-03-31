035b02b759afc89e2db38fe8ae262c32da2f443d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 19:06:18 +0530

    navbar reacts to current user

--- awesome-python/flask/flask_web_tuto/website/auth.py
+++ awesome-python/flask/flask_web_tuto/website/auth.py
@@ -26,7 +26,7 @@ def login():
         else:
             flash('User does not exist', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
@@ -68,4 +68,4 @@ def sign_up():
 
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
--- awesome-python/flask/flask_web_tuto/website/views.py
+++ awesome-python/flask/flask_web_tuto/website/views.py
@@ -7,4 +7,4 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-    return render_template('home.html')
+    return render_template('home.html', user=current_user)
