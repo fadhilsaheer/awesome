7e7958b6a21cf52f5d4d317dc39e1d881c4cf8de
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 16:12:47 +0530

    create user in db

--- awesome-python/flask/flask_web_tuto/website/auth.py
+++ awesome-python/flask/flask_web_tuto/website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -31,6 +34,11 @@ def sign_up():
         #     flash("Password is too short", category='error')
 
         else:
+            new_user = User(email=email, name=name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash("Account created !", category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
