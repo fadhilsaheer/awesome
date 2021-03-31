51c9deef9772e30d49ff33a0c072aa503cec262b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 16:03:13 +0530

    created database

--- awesome-python/flask/flask_web_tuto/website/__init__.py
+++ awesome-python/flask/flask_web_tuto/website/__init__.py
@@ -18,8 +18,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import Note
-    from .models import User
+    from .models import Note, User
 
     create_database(app)
 
