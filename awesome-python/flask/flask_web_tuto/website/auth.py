327fc530d7f1807f0507f532a1e4f5a7be1266c6
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 05:42:33 +0530

    added render template

--- awesome-python/flask/flask_web_tuto/website/auth.py
+++ awesome-python/flask/flask_web_tuto/website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -15,4 +15,4 @@ def logout():
 
 @auth.route('/signup')
 def sign_up():
-    return "<p>signup</p>"
+    return render_template("sign_up.html")
