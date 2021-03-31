8546cd6f24f304b048d73eda7e88b88eb0dbc4b4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 06:49:33 +0530

    created models

---
+++ awesome-python/flask/flask_web_tuto/website/models.py
@@ -0,0 +1,16 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Notes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(255))
