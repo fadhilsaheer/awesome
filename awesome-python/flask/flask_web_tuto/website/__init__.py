8636e0ae38118bf7ca1dbff47fb2e114e92fbf62
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 16:34:52 +0530

    added login manager

--- awesome-python/flask/flask_web_tuto/website/__init__.py
+++ awesome-python/flask/flask_web_tuto/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -11,7 +12,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'follow-me-and-star-my-repo-😉'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
     from .views import views
     from .auth import auth
 
@@ -21,6 +21,14 @@ def create_app():
     from .models import Note, User
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader()
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
