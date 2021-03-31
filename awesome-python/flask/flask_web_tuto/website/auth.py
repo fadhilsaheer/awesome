2fe22fbc8c210e434642edaba884a8feaaffd457
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 06:22:52 +0530

    added server check

--- awesome-python/flask/flask_web_tuto/website/auth.py
+++ awesome-python/flask/flask_web_tuto/website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -13,6 +13,24 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(name) < 3:
+            flash("Your name is too short", category='error')
+
+        elif password1 != password2:
+            flash("Password don't match", category='error')
+
+        # elif len(password1) < 7:
+        #     flash("Password is too short", category='error')
+
+        else:
+            flash("Account created !", category='success')
+
     return render_template("sign_up.html")
