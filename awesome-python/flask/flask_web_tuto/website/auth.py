8c9d2b14c77e4999c07445452e7f452506f00ec4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 16:29:32 +0530

    login_user flask login

--- awesome-python/flask/flask_web_tuto/website/auth.py
+++ awesome-python/flask/flask_web_tuto/website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -16,8 +17,10 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully !', category='success')
+                login_user(user, remember=True)
+
                 return redirect(url_for('views.home'))
-            
+
             else:
                 flash('Invalid credentials !', category='error')
         else:
@@ -59,6 +62,8 @@ def sign_up():
             db.session.commit()
 
             flash("Account created !", category='success')
+            login_user(user, remember=True)
+
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
