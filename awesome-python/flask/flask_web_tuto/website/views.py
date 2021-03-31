938114f29044a597512df7f67d61f384063590d7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 19:27:43 +0530

    add note

--- awesome-python/flask/flask_web_tuto/website/views.py
+++ awesome-python/flask/flask_web_tuto/website/views.py
@@ -1,10 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short !', category='error')
+            
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+
+            flash('Note added !', category='success')
+
     return render_template('home.html', user=current_user)
