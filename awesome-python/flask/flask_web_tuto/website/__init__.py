26035f29d900f5dd30ba5e8cee44bb71814eb151
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 06:39:31 +0530

    setup db

--- awesome-python/flask/flask_web_tuto/website/__init__.py
+++ awesome-python/flask/flask_web_tuto/website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'follow-me-and-star-my-repo-😉'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
