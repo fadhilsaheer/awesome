75f4538d117d538377d6213f135a82af2ac7634d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-03-31 18:53:21 +0530

    working on unicode bug

--- awesome-python/flask/flask_web_tuto/website/__init__.py
+++ awesome-python/flask/flask_web_tuto/website/__init__.py
@@ -26,7 +26,7 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    @login_manager.user_loader()
+    @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
