f7498e53604696b7cf284e0df0ce96d85d1dcd81
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-02 08:30:52 +0530

    created search bar

--- awesome-flutter/apps/food_delivery/lib/screens/home_screen.dart
+++ awesome-flutter/apps/food_delivery/lib/screens/home_screen.dart
@@ -28,6 +28,37 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                hintText: 'Search Food Or Restaurants',
+                prefixIcon: Icon(Icons.search, size: 30.0),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
