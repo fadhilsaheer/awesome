f90faae7cd4963cb6c35ae62eb588f8503600609
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-02 08:23:37 +0530

    created app bar

--- awesome-flutter/apps/food_delivery/lib/screens/home_screen.dart
+++ awesome-flutter/apps/food_delivery/lib/screens/home_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/data/data.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.account_circle,
+            size: 30.0,
+          ),
+          onPressed: () {},
+        ),
+        title: Text("Food Delivery"),
+        centerTitle: true,
+        actions: [
+          FlatButton(
+            onPressed: () {},
+            child: Text(
+              'Cart ${currentUser.cart.length}',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
