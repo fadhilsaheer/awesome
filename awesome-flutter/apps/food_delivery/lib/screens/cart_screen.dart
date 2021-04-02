2b1f3eef84039e8e0f22ec84e6af993c97d1bfd7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-02 15:54:31 +0530

    addded some pading and overflow ellippsis

--- awesome-flutter/apps/food_delivery/lib/screens/cart_screen.dart
+++ awesome-flutter/apps/food_delivery/lib/screens/cart_screen.dart
@@ -14,40 +14,49 @@ class _CartScreenState extends State<CartScreen> {
       height: 170.0,
       child: Row(
         children: [
-          Row(
-            children: [
-              Container(
-                width: 150.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(order.food.imageUrl),
-                    fit: BoxFit.cover,
+          Expanded(
+            child: Row(
+              children: [
+                Container(
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(order.food.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                  borderRadius: BorderRadius.circular(15.0),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    order.food.name,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ],
                     ),
                   ),
-                  SizedBox(height: 10.0),
-                  Text(
-                    order.restaurant.name,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ],
       ),
