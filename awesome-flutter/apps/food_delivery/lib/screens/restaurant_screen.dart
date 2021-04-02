5d7cdfec303c799aa2edf831d3204ccc7fdbe7b8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-02 10:14:34 +0530

    adde image and gradient

--- awesome-flutter/apps/food_delivery/lib/screens/restaurant_screen.dart
+++ awesome-flutter/apps/food_delivery/lib/screens/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/data/data.dart';
+import 'package:food_delivery/models/food.dart';
 import 'package:food_delivery/models/restaurant.dart';
 import 'package:food_delivery/widgets/rating_stars.dart';
 
@@ -15,6 +16,49 @@ class RestaurantScreen extends StatefulWidget {
 class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
+    _buildMenuItem(Food menuItem) {
+      return Center(
+        child: Stack(
+          children: [
+            Container(
+              height: 175.0,
+              width: 175.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(menuItem.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            Container(
+              height: 175.0,
+              width: 175.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.black.withOpacity(0.3),
+                    Colors.black87.withOpacity(0.3),
+                    Colors.black54.withOpacity(0.3),
+                    Colors.black38.withOpacity(0.3),
+                  ],
+                  stops: [
+                    0.1,
+                    0.4,
+                    0.6,
+                    0.9,
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       body: Column(
         children: [
@@ -119,9 +163,34 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                     fontSize: 20.0,
                   ),
                 ),
-              )
+              ),
             ],
-          )
+          ),
+          SizedBox(height: 10.0),
+          Center(
+            child: Text(
+              "Menu",
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.all(10.0),
+              crossAxisCount: 2,
+              children: List.generate(
+                widget.restaurant.menu.length,
+                (int index) {
+                  Food food = widget.restaurant.menu[index];
+                  return _buildMenuItem(food);
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
