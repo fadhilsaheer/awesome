57286ccf867825ed778dd8e988b2f8f8b8f5eb3f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-02 09:52:15 +0530

    added stack with icon and mage

--- awesome-flutter/apps/food_delivery/lib/screens/restaurant_screen.dart
+++ awesome-flutter/apps/food_delivery/lib/screens/restaurant_screen.dart
@@ -13,6 +13,44 @@ class RestaurantScreen extends StatefulWidget {
 class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                image: AssetImage(widget.restaurant.imageUrl),
+                height: 220.0,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 50.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      onPressed: () => Navigator.pop(context),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      onPressed: () {},
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 35.0,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
