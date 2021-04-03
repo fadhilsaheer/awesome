c7feb06a0c12f39ae0bc48ddb45076d747c27a34
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-03 17:41:54 +0530

    added sort

--- awesome-js/awesome-app/social_media_merng/graphql/resolvers/posts.js
+++ awesome-js/awesome-app/social_media_merng/graphql/resolvers/posts.js
@@ -5,7 +5,7 @@ module.exports = {
     Query: {
         async getPosts(){
             try{
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             }catch(err){
                 throw new Error(err);
