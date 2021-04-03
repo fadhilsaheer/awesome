a30f4bf4b8b92aa1be467231f0bdf3e20ad4c97e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-03 17:16:38 +0530

    added get post by id

--- awesome-js/awesome-app/social_media_merng/graphql/resolvers/posts.js
+++ awesome-js/awesome-app/social_media_merng/graphql/resolvers/posts.js
@@ -9,6 +9,18 @@ module.exports = {
             }catch(err){
                 throw new Error(err);
             }
+        },
+        async getPost(_, { postId }){
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    return post;
+                }else{
+                    throw new Error("Post not found");
+                }
+            }catch(err){
+                throw new Error(err);
+            }
         }
     }
 }
