4811bdbcd84eee8652d3bc5b2c6233c0636dcc05
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-03 06:29:16 +0530

    added index code and start script

---
+++ awesome-js/awesome-app/social_media_merng/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag")
+
+const typeDefs =  gql`
+    type Query{
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => "Hello world"
+    }
+}
+
+const server = new ApolloServer({typeDefs,resolvers});
+
+
+const port = process.env.PORT || 5000;
+server.listen({port}).then(({ url }) => {
+    console.log(`server started at port ${url}`);
+})
