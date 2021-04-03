d811a1c20f613e70cb87e97c5636bdc636b497bf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-03 17:32:39 +0530

    added function for check auth

---
+++ awesome-js/awesome-app/social_media_merng/utils/checkAuth.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const { AuthenticationError } = require("apollo-server");
+
+module.exports = (context) => {
+    const authHeader = context.req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split('Bearer ')[1]; // getting token from header
+        if(token){
+            try{
+                const user = jwt.verify(token, process.env.jwtKey)
+            }catch(err){
+                throw new AuthenticationError('Invalid/Expired token');
+            }
+        }else{
+            throw new Error("Authentication must be 'Bearer [token]'")
+        }
+    }else{
+        throw new Error("Authentication header must be provided")
+    }
+}
