c0f61b27918104a601049b839a556afc5345276d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-03 18:30:12 +0530

    like

--- awesome-js/awesome-app/social_media_merng/graphql/resolvers/posts.js
+++ awesome-js/awesome-app/social_media_merng/graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server-errors");
+const { AuthenticationError, UserInputError } = require("apollo-server-errors");
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/checkAuth");
 
@@ -55,6 +55,28 @@ module.exports = {
                 }
             } catch (err){
                 throw new Error(err)
+            }
+        },
+        
+        async likePost(_, { postId }, context){
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    post.likes = post.likes.filter(like => like.username !== username);
+                }else{
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString(),
+                    })
+                }
+
+                await post.save();
+                return post;
+            }else{
+                throw new UserInputError("Post not found");
             }
         }
     }
