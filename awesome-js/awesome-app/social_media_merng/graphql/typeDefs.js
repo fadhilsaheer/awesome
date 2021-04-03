3199e94b7d13c859456587501dc11e887035ed9b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-03 17:14:27 +0530

    added type defs for post

--- awesome-js/awesome-app/social_media_merng/graphql/typeDefs.js
+++ awesome-js/awesome-app/social_media_merng/graphql/typeDefs.js
@@ -21,11 +21,14 @@ const typeDefs =  gql`
         email: String!,
     }
     type Query{
-        getPosts: [Post]
+        getPosts: [Post],
+        getPost(postId: ID!): Post,
     }
     type Mutation{
         register(registerInput: RegisterInput): User!,
-        login(username: String!, password: String!): User!
+        login(username: String!, password: String!): User!,
+        createPost(body: String!): Post!,
+        deletePost(postId: ID!): String!,
     }
 `
 
