cbe564fc9ab0f66225862a1404fece5ad909183e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-04 19:04:46 +0530

    fixed created at in comment

--- awesome-js/awesome-app/social_media_merng/graphql/resolvers/comments.js
+++ awesome-js/awesome-app/social_media_merng/graphql/resolvers/comments.js
@@ -17,10 +17,11 @@ module.exports = {
             const post = await Post.findById(postId);
 
             if(post){
+
                 post.comments.unshift({
                     body,
                     username,
-                    createdAt: new Date().toISOString()
+                    createdAt: new Date().toISOString(),
                 });
 
                 await post.save();
--- awesome-js/awesome-app/social_media_merng/models/Post.js
+++ awesome-js/awesome-app/social_media_merng/models/Post.js
@@ -8,6 +8,7 @@ const postSchema = new Schema({
         {
             body: String,
             username: String,
+            createdAt: String,
         }
     ],
     likes: [
