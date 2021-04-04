0cf2047031446a0b9f04c780d57056c3bf2af121
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-04 18:23:02 +0530

    added delete button

--- awesome-js/awesome-app/social_media_merng/client/src/components/DeleteButton.jsx
+++ awesome-js/awesome-app/social_media_merng/client/src/components/DeleteButton.jsx
@@ -1,11 +1,38 @@
-import { Button, Icon } from 'semantic-ui-react';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { Button, Icon, Confirm } from 'semantic-ui-react';
+import gql from 'graphql-tag';
 
 const DeleteButton = ({ postId }) => {
+    const [confirmOpen, setConfirmOpen] = useState(false);
+
+    const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+        variables: { postId },
+        update() {
+            setConfirmOpen(false);
+        }
+    });
+
+
+
     return (
-        <Button as="div" color="red" floated="right">
-            <Icon name="trash" style={{ margin: 0 }} />
-        </Button>
+        <>
+            <Button as="div" color="red" floated="right" onClick={()=> setConfirmOpen(true)}>
+                <Icon name="trash" style={{ margin: 0 }} />
+            </Button>
+            <Confirm 
+                open={confirmOpen} 
+                onCancel={()=> setConfirmOpen(false)}
+                onConfirm={deletePost}
+            />
+        </>
     );
 }
+
+const DELETE_POST_MUTATION = gql`
+    mutation deletePost($postId: ID!){
+        deletePost(postId: $postId)
+    }
+`
 
 export default DeleteButton;
