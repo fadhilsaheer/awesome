ecf16705d00f503443b11e00deb1e913eece821b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-04 07:31:01 +0530

    completed navbar

--- awesome-js/awesome-app/social_media_merng/client/src/components/MenuBar.jsx
+++ awesome-js/awesome-app/social_media_merng/client/src/components/MenuBar.jsx
@@ -3,12 +3,16 @@ import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 const MenuBar = () => {
-    const [activeItem, setActiveItem] = useState("home");
+    const pathname = window.location.pathname;
+    const path = pathname === '/' ? 'home' : pathname.substr(1);
+
+    const [activeItem, setActiveItem] = useState(path);
+
 
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
     return (
-        <Menu pointing secondary>
+        <Menu pointing secondary size="massive" color="teal">
             <Menu.Item name='home' active={activeItem === 'home'} onClick={handleItemClick} as={Link} to="/" />
             <Menu.Menu position='right'>
                 <Menu.Item name='login' active={activeItem === 'login'} onClick={handleItemClick} as={Link} to="/login" />
