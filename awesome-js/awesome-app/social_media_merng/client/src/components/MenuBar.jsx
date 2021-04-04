8ce27a22af408ec3f10ec4cda4684b73479a2a2c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-04 10:05:23 +0530

    added custom menubar based on user

--- awesome-js/awesome-app/social_media_merng/client/src/components/MenuBar.jsx
+++ awesome-js/awesome-app/social_media_merng/client/src/components/MenuBar.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 const MenuBar = () => {
+    const { user, logout } = useContext(AuthContext);
     const pathname = window.location.pathname;
     const path = pathname === '/' ? 'home' : pathname.substr(1);
 
@@ -11,7 +14,14 @@ const MenuBar = () => {
 
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
+    const menuBar = user ? (
+        <Menu pointing secondary size="massive" color="teal">
+            <Menu.Item name={user.username} active as={Link} to="/" />
+            <Menu.Menu position='right'>
+                <Menu.Item name='logout' onClick={logout} />
+            </Menu.Menu>
+        </Menu>
+    ) : (
         <Menu pointing secondary size="massive" color="teal">
             <Menu.Item name='home' active={activeItem === 'home'} onClick={handleItemClick} as={Link} to="/" />
             <Menu.Menu position='right'>
@@ -20,6 +30,8 @@ const MenuBar = () => {
             </Menu.Menu>
         </Menu>
     );
+
+    return menuBar;
 }
 
 export default MenuBar;
