3d78a6c024bf8c6d841cb45f3d9df9181c0d80d0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-04 17:14:10 +0530

    added transition

--- awesome-js/awesome-app/social_media_merng/client/src/pages/Home.jsx
+++ awesome-js/awesome-app/social_media_merng/client/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/auth';
 import { useQuery } from '@apollo/client';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Transition } from 'semantic-ui-react';
 import { FETCH_QUERY_POST } from '../utils/graphql';
 
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
 
 const Home = () => {
-    const { user } = useContext(AuthContext); 
+    const { user } = useContext(AuthContext);
     const { loading, data } = useQuery(FETCH_QUERY_POST);
     const posts = data?.getPosts;
 
@@ -26,11 +26,13 @@ const Home = () => {
                 {loading ? (
                     <h2>Loading posts .....</h2>
                 ) : (
-                    posts && posts.map(post => (
-                        <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                            <PostCard post={post} />
-                        </Grid.Column>
-                    ))
+                    <Transition.Group>
+                        {posts && posts.map(post => (
+                            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                                <PostCard post={post} />
+                            </Grid.Column>
+                        ))}
+                    </Transition.Group>
                 )}
             </Grid.Row>
         </Grid>
