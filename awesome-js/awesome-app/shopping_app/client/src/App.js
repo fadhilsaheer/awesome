4b548b1147e62335a8429e99fe2ee67019ba9df9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-05 10:56:51 +0530

    auth route

--- awesome-js/awesome-app/shopping_app/client/src/App.js
+++ awesome-js/awesome-app/shopping_app/client/src/App.js
@@ -1,18 +1,25 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+import { Container } from "@material-ui/core";
 
-import Home from './pages/home';
-import Login from './pages/Login';
-import Register from './pages/register';
-import Item from './pages/item';
+import AuthRoute from "./utils/AuthRoute";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Item from "./pages/item";
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/item/:id" component={Item} />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/item/:id" component={Item} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ awesome-js/awesome-app/shopping_app/client/src/utils/AuthRoute.jsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { AuthContext } from '../context/auth';
+
+function AuthRoute({ component: Component, ...rest }) {
+    const { user } = useContext(AuthContext);
+    
+    return <Route
+        {...rest}
+        render={props => user ? <Redirect to="/" /> : <Component {...props} />}  
+    />
+}
+
+export default AuthRoute
