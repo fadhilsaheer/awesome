be5e53292836dc417f8d439ad568929a70fdb885
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-05 07:32:48 +0530

    completed header

--- awesome-js/awesome-next/whatsapp-clone/components/Sidebar.jsx
+++ awesome-js/awesome-next/whatsapp-clone/components/Sidebar.jsx
@@ -1,16 +1,50 @@
-import styled from "styled-components"
+import { Avatar, IconButton } from "@material-ui/core";
+import styled from 'styled-components'
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
     return (
         <Container>
             <Header>
-                
+                <UserAvatar />
+
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </IconsContainer>
             </Header>
         </Container>
     )
 }
 
-export default Sidebar
+export default Sidebar;
 
 const Container = styled.div``;
-const Header = styled.div``;
+
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+    transition: 0.5s;
+    :hover {
+        opacity: 0.8;
+    }
+`;
+
+const IconsContainer = styled.div``;
