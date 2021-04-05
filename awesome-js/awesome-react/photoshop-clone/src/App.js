a387370dd6515839f1b5699b48afd25a35e5f3d5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-05 15:29:35 +0530

    added data

--- awesome-js/awesome-react/photoshop-clone/src/App.js
+++ awesome-js/awesome-react/photoshop-clone/src/App.js
@@ -2,6 +2,79 @@ import "./App.css";
 import Slider from "./Slider";
 import SidebarItem from "./SidebarItem";
 
+const DEFAULT_OPTIONS = [
+  {
+    name: "Brightness",
+    property: "brightness",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  },
+  {
+    name: "Contrast",
+    property: "contrast",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  },
+  {
+    name: "Saturation",
+    property: "saturation",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  },
+  {
+    name: "Grayscale",
+    property: "grayscale",
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: "%",
+  },
+  {
+    name: "Sepia",
+    property: "sepia",
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: "%",
+  },
+  {
+    name: "Hue Rotate",
+    property: "hue-rotate",
+    value: 0,
+    range: {
+      min: 0,
+      max: 360,
+    },
+    unit: "deg",
+  },
+  {
+    name: "Blur",
+    property: "blur",
+    value: 0,
+    range: {
+      min: 0,
+      max: 20,
+    },
+    unit: "px",
+  },
+];
+
 function App() {
   return (
     <div className="container">
