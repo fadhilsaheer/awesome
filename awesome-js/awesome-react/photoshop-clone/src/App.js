778ee19dc6f6b97db8e0a8ed233c62cdf345b744
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-05 15:10:33 +0530

    completed slider

--- awesome-js/awesome-react/photoshop-clone/src/App.js
+++ awesome-js/awesome-react/photoshop-clone/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Slider from './Slider';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="main-image" />
+      <div className="sidebar">sidebar</div>
+      <Slider />
     </div>
   );
 }
---
+++ awesome-js/awesome-react/photoshop-clone/src/Slider.js
@@ -0,0 +1,10 @@
+
+function Slider() {
+    return (
+        <div className="slider-container">
+            <input type="range" className="slider" />
+        </div>
+    )
+}
+
+export default Slider
