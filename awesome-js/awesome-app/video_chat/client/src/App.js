778a6c86f1a696ccae1716533303f00a5699184e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 10:52:54 +0530

    added styles for home page

--- awesome-js/awesome-app/video_chat/client/src/App.js
+++ awesome-js/awesome-app/video_chat/client/src/App.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles'
 
 import VideoPlayer from './components/VideoPlayer';
 import Notifications from './components/Notifications';
 import Options from './components/Options';
 
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        borderRadius: 15,
+        margin: '30px 100px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '600px',
+        border: '2px solid black',
+    
+        [theme.breakpoints.down('xs')]: {
+          width: '90%',
+        },
+      },
+      image: {
+        marginLeft: '15px',
+      },
+      wrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+      },
+}));
+
 const App = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            <AppBar position="static" color="inherit">
+        <div className={classes.wrapper}>
+            <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography variant="h2" align="center">Video Chat</Typography>
             </AppBar>
             <VideoPlayer />
