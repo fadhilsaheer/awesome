3270030aaf902547d363e10c1e966eeaea8e2095
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 15:19:35 +0530

    completed setup

--- awesome-python/projects/snake_game/main.py
+++ awesome-python/projects/snake_game/main.py
@@ -17,3 +17,19 @@ pygame.display.set_caption("Awesome snake game")
 
 clock = pygame.time.Clock()
 
+snake_size = 10
+snake_speed = 15
+
+message_font = pygame.font.SysFont('ubuntu', 30)
+score_font = pygame.font.SysFont('ubuntu', 25)
+
+
+def print_score(score):
+    text = score_font.render(f"Score: {str(score)}", True, orange)
+    game_display.blit(text, [0, 0])
+
+
+def draw_snake(snake_size, snake_pixels):
+    for pixel in snake_pixels:
+        pygame.draw.rect(game_display, white, [pixel[0], pixel[1], snake_size, snake_size])
+
