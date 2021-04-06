ae08fd85fec4d25310dbfedfcdb8d8a83e784095
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 15:30:21 +0530

    snake core

--- awesome-python/projects/snake_game/main.py
+++ awesome-python/projects/snake_game/main.py
@@ -33,3 +33,46 @@ def draw_snake(snake_size, snake_pixels):
     for pixel in snake_pixels:
         pygame.draw.rect(game_display, white, [pixel[0], pixel[1], snake_size, snake_size])
 
+
+def run_game():
+    game_over = False
+    game_close = False
+
+    x = width / 2
+    y = height / 2
+
+    x_speed = 0
+    y_speed = 0
+
+    snake_pixels = []
+    snake_length = 1
+
+    target_x = round(random.randrange(0, width - snake_size) / 10.0) * 10
+    target_y = round(random.randrange(0, height - snake_size) / 10.0) * 10
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_speed = -snake_size
+                    y_speed = 0
+
+                if event.key == pygame.K_RIGHT:
+                    x_speed = snake_size
+                    y_speed = 0
+
+                if event.key == pygame.K_UP:
+                    x_speed = 0
+                    y_speed = -snake_size
+
+                if event.key == pygame.K_DOWN:
+                    x_speed = 0
+                    y_speed = snake_size
+
+        if x >= width or x < 0 or y >= height or y < 0:
+            game_close = True
+
+        x += x_speed
+        y += y_speed
