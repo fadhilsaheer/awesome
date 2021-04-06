37109848beab05b43818c7b2b89d15e7dba9a642
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 10:29:52 +0530

    created socket connection in server

--- awesome-js/awesome-app/video_chat/index.js
+++ awesome-js/awesome-app/video_chat/index.js
@@ -17,4 +17,23 @@ app.get('/', (req, res)=>{
     res.send("Server is running")
 });
 
+io.on('connection', socket => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('callended');
+    });
+
+    socket.on("calluser", ({ userToCall, signalData, fromName })=>{
+        io.to(userToCall).emit('callUser', { signal: signalData, from: fromName });
+    })
+
+    socket.on("answercall", data => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    })
+
+});
+
+
+
 server.listen(port, ()=> console.log(`server listening on port ${port}`));
