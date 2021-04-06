004547052273902263585b679802ad6d89d6f83a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 15:12:41 +0530

    initalized pygame

---
+++ awesome-python/projects/snake_game/main.py
@@ -0,0 +1,19 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+# Define colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+orange = (255, 165, 0)
+
+width, height = 600, 400
+
+game_display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Awesome snake game")
+
+clock = pygame.time.Clock()
+
