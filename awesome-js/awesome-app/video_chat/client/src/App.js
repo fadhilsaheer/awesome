d9d6614515634f65a3ff4ef022afad6bb436ca54
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 10:42:01 +0530

    ...

--- awesome-js/awesome-app/video_chat/client/src/App.js
+++ awesome-js/awesome-app/video_chat/client/src/App.js
@@ -1,7 +1,9 @@
-function App() {
+import React from 'react'
+
+const App = () => {
     return (
         <div>
-            
+            Video chat app   
         </div>
     )
 }
--- awesome-js/awesome-app/video_chat/client/src/index.js
+++ awesome-js/awesome-app/video_chat/client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import './style.css'
+import './styles.css'
 
 ReactDOM.render(<App />, document.getElementById("root"));
