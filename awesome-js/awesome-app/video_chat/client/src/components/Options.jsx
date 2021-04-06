8e655001b90358a9a8619fafc01d844e6bdf1049
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 11:41:33 +0530

    added styles for option controller

--- awesome-js/awesome-app/video_chat/client/src/components/Options.jsx
+++ awesome-js/awesome-app/video_chat/client/src/components/Options.jsx
@@ -1,9 +1,50 @@
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import { useContext } from 'react';
+import { SocketContext } from '../SocketContext';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    gridContainer: {
+        width: '100%',
+        [theme.breakpoints.down('xs')]: {
+            flexDirection: 'column',
+        },
+    },
+    container: {
+        width: '600px',
+        margin: '35px 0',
+        padding: 0,
+        [theme.breakpoints.down('xs')]: {
+            width: '80%',
+        },
+    },
+    margin: {
+        marginTop: 20,
+    },
+    padding: {
+        padding: 20,
+    },
+    paper: {
+        padding: '10px 20px',
+        border: '2px solid black',
+    },
+}));
+
 const Options = ({ children }) => {
+    const classes = useStyles();
+
     return (
         <div>
             {children}
         </div>
     );
 }
- 
+
 export default Options;
