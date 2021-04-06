e897abd6bf0551d43bc12b6b6a03cd60d9fafdcd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-06 14:36:05 +0530

    fixed server bug

--- awesome-js/awesome-app/video_chat/index.js
+++ awesome-js/awesome-app/video_chat/index.js
@@ -1,36 +1,37 @@
+  
 const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
 });
 
 app.use(cors());
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.get('/', (req, res) => {
+	res.send('Running');
 });
 
 io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
+	socket.emit("me", socket.id);
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callended");
-  });
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
 
-  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
-  });
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
 
-  socket.on("answercall", (data) => {
-    io.to(data.to).emit("callaccepted", data.signal);
-  });
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
 });
 
-server.listen(port, () => console.log(`server listening on port ${port}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
