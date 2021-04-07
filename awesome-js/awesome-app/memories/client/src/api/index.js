e22cc41b0e8da54eb990ea480d45d6f415995052
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-07 11:40:05 +0530

    create a post

--- awesome-js/awesome-app/memories/client/src/api/index.js
+++ awesome-js/awesome-app/memories/client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/'; // feel free to change it
+const url = 'http://localhost:5000/posts'; // feel free to change it
 
 export const fetchPosts = () => axios.get(url);
 
--- awesome-js/awesome-app/memories/client/src/reducers/posts.js
+++ awesome-js/awesome-app/memories/client/src/reducers/posts.js
@@ -3,7 +3,7 @@ const reducer = (posts = [], action) => {
         case 'FETCH ALL':
             return action.payload;
         case 'CREATE':
-            return posts;
+            return [...posts, action.payload]
         default:
             return posts;
     }
