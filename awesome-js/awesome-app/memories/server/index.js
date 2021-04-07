a62efc876e53c6996da1248fbb87655fa63eec22
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-07 11:11:01 +0530

    fixed cors bug

--- awesome-js/awesome-app/memories/server/index.js
+++ awesome-js/awesome-app/memories/server/index.js
@@ -10,11 +10,12 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = process.env.database;
 const PORT = process.env.PORT || 5000;
