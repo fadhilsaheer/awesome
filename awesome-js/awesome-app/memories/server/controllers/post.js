7e5f76236e9f86eeaaa1a9bc1e69434342c0a7c3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-07 09:59:30 +0530

    create post and get post controllers

--- awesome-js/awesome-app/memories/server/controllers/post.js
+++ awesome-js/awesome-app/memories/server/controllers/post.js
@@ -1,3 +1,26 @@
-export const getPosts = (req, res) => {
-    res.send("Hello world");
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {   
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+    
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
--- awesome-js/awesome-app/memories/server/routes/posts.js
+++ awesome-js/awesome-app/memories/server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getPosts } from '../controllers/post.js';
+import { getPosts, createPost } from '../controllers/post.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost);
 
 export default router;
