7c1d47983b16294afcdd4b9fa9f67d1570b3393b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-07 09:49:16 +0530

    folder structure

--- awesome-js/awesome-app/memories/server/index.js
+++ awesome-js/awesome-app/memories/server/index.js
@@ -6,7 +6,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
---
+++ awesome-js/awesome-app/memories/server/routes/posts.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { getPosts } from '../controllers/post.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+
+export default router;
