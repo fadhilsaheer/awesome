711c7511b3a5e005ab18b810535d46b1ed607b28
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-07 08:31:22 +0530

    status circle

--- awesome-flutter/apps/budget_app/lib/screens/category_screen.dart
+++ awesome-flutter/apps/budget_app/lib/screens/category_screen.dart
@@ -1,4 +1,7 @@
+import 'package:budget_app/helpers/color_helper.dart';
 import 'package:budget_app/models/category_model.dart';
+import 'package:budget_app/models/expense_model.dart';
+import 'package:budget_app/widgets/radial_painter.dart';
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -12,6 +15,14 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category.name),
@@ -23,6 +34,46 @@ class _CategoryScreenState extends State<CategoryScreen> {
             iconSize: 30.0,
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0,
+                  )
+                ],
+              ),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200],
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                    '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
