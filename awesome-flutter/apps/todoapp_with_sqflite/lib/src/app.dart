56c348783bf9499b67933e012367a5c16ae989e1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-07 20:10:37 +0530

    basic cleanup and app bar

---
+++ awesome-flutter/apps/todoapp_with_sqflite/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp_with_sqflite/screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
