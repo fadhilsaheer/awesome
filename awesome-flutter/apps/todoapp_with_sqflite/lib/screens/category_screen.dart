aace4592a1bb218b91dd1dded4c38acc905775c0
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-08 07:45:10 +0530

    alert dialog & floating action button

--- awesome-flutter/apps/todoapp_with_sqflite/lib/screens/category_screen.dart
+++ awesome-flutter/apps/todoapp_with_sqflite/lib/screens/category_screen.dart
@@ -6,8 +6,69 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actions: [
+            FlatButton(
+              child: Text("Cancel"),
+              onPressed: () => Navigator.pop(context),
+              color: Colors.red,
+            ),
+            FlatButton(
+              child: Text("Save"),
+              onPressed: () {},
+              color: Colors.blue,
+            ),
+          ],
+          title: Text("Categories Form"),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write a category',
+                    labelText: 'Category',
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write a description',
+                    labelText: 'Description',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.arrow_back, color: Colors.white),
+          color: Colors.blue,
+          elevation: 0.0,
+        ),
+        title: Text("Categories"),
+      ),
+      body: Center(
+        child: Text("Welcome to category screen"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _showFormDialog(context),
+      ),
+    );
   }
 }
