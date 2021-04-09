2c94d1563975a8cd227acc1e11b9c3510ae401f8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-09 15:22:37 +0530

    created post card ui

--- awesome-js/awesome-app/memories/client/src/components/posts/post/Post.js
+++ awesome-js/awesome-app/memories/client/src/components/posts/post/Post.js
@@ -1,11 +1,47 @@
 import React from "react";
 import useStyles from "./style";
-function Post() {
+
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  CardActions,
+} from "@material-ui/core";
+
+import ThumbUpAlt from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
+import moment from "moment";
+
+function Post({ post }) {
   const classes = useStyles();
+
   return (
-    <div>
-      <h1>Post</h1>
-    </div>
+     <Card className={classes.card}>
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: 'white' }} size="small" onClick={() => {}}><MoreHorizIcon fontSize="default" /></Button>
+      </div>
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+      </div>
+      <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={()=> {}}><ThumbUpAlt fontSize="small" /> Like {post.likeCount} </Button>
+        <Button size="small" color="primary" onClick={()=> {}}><DeleteIcon fontSize="small" /> Delete</Button>
+      </CardActions>
+    </Card>
   );
 }
 
