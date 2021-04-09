d3e288ddf90ddc6da195de13510726718968bc62
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-09 15:34:06 +0530

    update route

--- awesome-js/awesome-app/memories/server/controllers/post.js
+++ awesome-js/awesome-app/memories/server/controllers/post.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -24,3 +25,15 @@ export const createPost = async (req, res) => {
     }
 }
 
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).send("No post with that id");
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.jon(updatePost);
+}
