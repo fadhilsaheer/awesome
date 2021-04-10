8a8734a01df3a49fe0a19dc23877eeed88f707bf
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-10 21:20:34 +0530

    added data and fixed import error

---
+++ awesome-flutter/apps/social_media_ui/lib/models/user_model.dart
@@ -0,0 +1,21 @@
+import 'package:social_media_ui/models/post_model.dart';
+
+class User {
+  final String profileImageUrl;
+  final String backgroundImageUrl;
+  final String name;
+  final int following;
+  final int followers;
+  final List<Post> posts;
+  final List<Post> favorites;
+
+  User({
+    this.profileImageUrl,
+    this.backgroundImageUrl,
+    this.name,
+    this.following,
+    this.followers,
+    this.posts,
+    this.favorites,
+  });
+}
