d8d657f5aeee60a23e6b11663b1b103203d29c9a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-10 21:39:12 +0530

    created title and input fields

--- awesome-flutter/apps/social_media_ui/lib/screens/login_screen.dart
+++ awesome-flutter/apps/social_media_ui/lib/screens/login_screen.dart
@@ -20,6 +20,54 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: MediaQuery.of(context).size.height / 2.5,
                 fit: BoxFit.cover,
               ),
+              Text(
+                'FRENZY',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 34.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.0,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 10.0,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Username',
+                    prefixIcon: Icon(
+                      Icons.account_box,
+                      size: 30.0,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 10.0,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30.0,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  ),
+                  obscureText: true,
+                ),
+              ),
             ],
           ),
         ),
