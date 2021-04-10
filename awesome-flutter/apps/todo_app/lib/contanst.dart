5756b159971b828018b8505be8a8787d9cb9cc0a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-10 07:16:11 +0530

    created home page and floating action button

---
+++ awesome-flutter/apps/todo_app/lib/contanst.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xff212730);
+Color secondaryColor = Color(0xff2c2f33);
+Color accentColor = Color(0xff1abc9c);
--- awesome-flutter/apps/todo_app/lib/screens/home_screen.dart
+++ awesome-flutter/apps/todo_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/contanst.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: primaryColor,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add, size: 30.0),
+        backgroundColor: accentColor,
+        onPressed: () {},
+      ),
+    );
   }
 }
