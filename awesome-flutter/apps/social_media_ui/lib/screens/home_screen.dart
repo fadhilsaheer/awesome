f29933cf1563a0dbff10076e9d814a5864af9006
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-10 22:09:36 +0530

    created tab bar

--- awesome-flutter/apps/social_media_ui/lib/screens/home_screen.dart
+++ awesome-flutter/apps/social_media_ui/lib/screens/home_screen.dart
@@ -5,7 +5,16 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +30,22 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.bold,
             letterSpacing: 10.0,
           ),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: Theme.of(context).primaryColor,
+          indicatorWeight: 3.0,
+          labelStyle: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontSize: 18.0,
+          ),
+          tabs: [
+            Tab(text: 'Trending'),
+            Tab(text: 'Latest'),
+          ],
         ),
       ),
     );
