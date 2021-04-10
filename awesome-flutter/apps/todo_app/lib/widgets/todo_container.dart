da2f60945b98a93335cefb05d1c84f9af25bcc69
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-10 18:09:15 +0530

    created checkbox

--- awesome-flutter/apps/todo_app/lib/widgets/todo_container.dart
+++ awesome-flutter/apps/todo_app/lib/widgets/todo_container.dart
@@ -15,7 +15,7 @@ class _TodoContainerState extends State<TodoContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 10.0),
+      margin: EdgeInsets.only(top: 15.0),
       padding: EdgeInsets.all(20.0),
       width: double.infinity,
       height: 100.0,
@@ -23,10 +23,29 @@ class _TodoContainerState extends State<TodoContainer> {
         color: secondayrColor,
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("My Awesome Todo"),
+          Text(
+            "My awesome todo",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Row(
+            children: [
+              Theme(
+                data: ThemeData(unselectedWidgetColor: accentColor),
+                child: Checkbox(
+                  value: true,
+                  activeColor: accentColor,
+                  onChanged: (val) {},
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
