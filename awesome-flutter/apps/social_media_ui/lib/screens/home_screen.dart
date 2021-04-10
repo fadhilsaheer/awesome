58228a143b7a46953ff638d10f32e972068b34a7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-10 22:05:14 +0530

    created app bar

--- awesome-flutter/apps/social_media_ui/lib/screens/home_screen.dart
+++ awesome-flutter/apps/social_media_ui/lib/screens/home_screen.dart
@@ -9,7 +9,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+        title: Text(
+          "FRENZY",
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
+          ),
+        ),
+      ),
     );
   }
 }
