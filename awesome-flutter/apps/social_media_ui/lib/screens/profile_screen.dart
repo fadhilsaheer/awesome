3ae50daff197916d35e8ae6990fe061da69fcc81
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-11 10:17:54 +0530

    created drawer in  profile screen

--- awesome-flutter/apps/social_media_ui/lib/screens/profile_screen.dart
+++ awesome-flutter/apps/social_media_ui/lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_ui/models/user_model.dart';
+import 'package:social_media_ui/widgets/custom_drawer.dart';
 
 class ProfileScreen extends StatefulWidget {
   final User user;
@@ -11,15 +12,36 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final GlobalKey<ScaffoldState> _scafolldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scafolldKey,
+      drawer: CustomDrawer(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(Image(
-              image: AssetImage(),
-            ))
+            Stack(
+              children: [
+                Image(
+                  image: AssetImage(widget.user.backgroundImageUrl),
+                  height: 300.0,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  top: 50.0,
+                  left: 20.0,
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    iconSize: 30.0,
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () => _scafolldKey.currentState.openDrawer(),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
