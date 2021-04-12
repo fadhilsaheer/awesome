aabbf59f6c12a3f850593f037eb2e113a2124f0e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-12 08:57:01 +0530

    added delete post functionality

--- awesome-js/awesome-app/memories/client/src/components/posts/post/Post.js
+++ awesome-js/awesome-app/memories/client/src/components/posts/post/Post.js
@@ -1,14 +1,9 @@
 import React from "react";
 import useStyles from "./style";
 
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Button,
-  Typography,
-  CardActions,
-} from "@material-ui/core";
+import { Card, CardContent, CardMedia, Button, Typography, CardActions } from "@material-ui/core";
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../actions/post';
 
 import ThumbUpAlt from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
@@ -18,19 +13,14 @@ import moment from "moment";
 
 function Post({ post, setCurrentId }) {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <Card className={classes.card}>
-      <CardMedia
-        className={classes.media}
-        image={post.selectedFile}
-        title={post.title}
-      />
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
-        <Typography variant="body2">
-          {moment(post.createdAt).fromNow()}
-        </Typography>
+        <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
       <div className={classes.overlay2}>
         <Button style={{ color: "white" }} size="small" onClick={() => setCurrentId(post._id)}>
@@ -38,28 +28,17 @@ function Post({ post, setCurrentId }) {
         </Button>
       </div>
       <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary" component="h2">
-          {post.tags.map((tag) => `#${tag} `)}
-        </Typography>
+        <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
-      <Typography
-        className={classes.title}
-        gutterBottom
-        variant="h5"
-        component="h2"
-      >
-        {post.title}
-      </Typography>
+      <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {post.message}
-        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" onClick={() => {}}>
           <ThumbUpAlt fontSize="small" /> Like {post.likeCount}{" "}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small" /> Delete
         </Button>
       </CardActions>
