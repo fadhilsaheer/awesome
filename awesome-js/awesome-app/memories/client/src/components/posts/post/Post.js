566982cc54a96dd78de0e107267c958506b4b2ae
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-12 07:52:25 +0530

    setCurrentId on post click

--- awesome-js/awesome-app/memories/client/src/components/posts/post/Post.js
+++ awesome-js/awesome-app/memories/client/src/components/posts/post/Post.js
@@ -17,7 +17,7 @@ import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 import moment from "moment";
 
-function Post({ post }) {
+function Post({ post, setCurrentId }) {
   const classes = useStyles();
 
   return (
@@ -34,7 +34,7 @@ function Post({ post }) {
         </Typography>
       </div>
       <div className={classes.overlay2}>
-        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+        <Button style={{ color: "white" }} size="small" onClick={() => setCurrentId(post._id)}>
           <MoreHorizIcon fontSize="default" />
         </Button>
       </div>
