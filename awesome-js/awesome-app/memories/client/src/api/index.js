b3ef275dd9e1b9506d7153e224d0680cbf042b25
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-12 07:54:48 +0530

    api fopr update post

--- awesome-js/awesome-app/memories/client/src/api/index.js
+++ awesome-js/awesome-app/memories/client/src/api/index.js
@@ -5,3 +5,5 @@ const url = 'http://localhost:5000/posts'; // feel free to change it
 export const fetchPosts = () => axios.get(url);
 
 export const createPosts = (newPost) => axios.post(url, newPost);
+
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatePost) 
