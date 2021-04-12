cfda8ee6fdb5ad0f738192abdb526e1d3b16225d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-12 08:27:50 +0530

    fixed updating bugs

--- awesome-js/awesome-app/memories/client/src/api/index.js
+++ awesome-js/awesome-app/memories/client/src/api/index.js
@@ -6,4 +6,4 @@ export const fetchPosts = () => axios.get(url);
 
 export const createPosts = (newPost) => axios.post(url, newPost);
 
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatePost) 
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost) 
--- awesome-js/awesome-app/memories/client/src/reducers/posts.js
+++ awesome-js/awesome-app/memories/client/src/reducers/posts.js
@@ -1,14 +1,16 @@
 const reducer = (posts = [], action) => {
-    switch(action.type){
-        case 'UPDATE':
-            return posts.map(post => post._id === action.payload._id ? action.payload : post)
-        case 'FETCH ALL':
-            return action.payload;
-        case 'CREATE':
-            return [...posts, action.payload]
-        default:
-            return posts;
-    }
-}
+  switch (action.type) {
+    case "FETCH ALL":
+      return action.payload;
+    case "CREATE":
+      return [...posts, action.payload];
+    case "UPDATE":
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    default:
+      return posts;
+  }
+};
 
 export default reducer;
