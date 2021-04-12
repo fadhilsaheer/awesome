68c728ddddfe6108a847f5089cae33d34b4655b7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-12 08:22:40 +0530

    fixed update bug

--- awesome-js/awesome-app/memories/server/controllers/post.js
+++ awesome-js/awesome-app/memories/server/controllers/post.js
@@ -26,14 +26,14 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-    const { id: _id } = req.params;
-    const post = req.body;
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    if(!mongoose.Types.ObjectId.isValid(_id)){
-        return res.status(404).send("No post with that id");
-    }
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
-    res.json(updatedPost);
+    res.json(updatedPost);;
 }
