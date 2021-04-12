255ee748f7f62d58234a0db612d718ac30bada49
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-12 08:47:22 +0530

    created function

--- awesome-js/awesome-app/memories/server/controllers/post.js
+++ awesome-js/awesome-app/memories/server/controllers/post.js
@@ -39,5 +39,11 @@ export const updatePost = async (req, res) => {
 }
 
 export const deletePost = async (req, res) => {
-    
+    const { id } = req.params;
+   
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully' });
 }
