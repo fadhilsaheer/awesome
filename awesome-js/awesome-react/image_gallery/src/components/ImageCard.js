93c2d3a15f5235074abbabc5c585c5621ca32337
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-13 14:44:41 +0530

    displayed dynamic data

--- awesome-js/awesome-react/image_gallery/src/components/ImageCard.js
+++ awesome-js/awesome-react/image_gallery/src/components/ImageCard.js
@@ -1,13 +1,13 @@
-function ImageCard() {
+function ImageCard({ image }) {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img src="https://source.unsplash.com/random" className="w-full" alt="awesome" />
+            <img src={image.webformatURL} className="w-full" alt={`pic by ${image.user}`} />
             <div className="px-6 py-4">
-                <div className="font-bold text-purple-500 text-xl mb-2">Photo by awesome</div>
+                <div className="font-bold text-purple-500 text-xl mb-2">Photo by {image.user}</div>
                 <ul>
-                    <li><strong>Views: </strong>4000</li>
-                    <li><strong>Downloads: </strong>300</li>
-                    <li><strong>Likes: </strong>400</li>
+                    <li><strong>Views: </strong>{image.views}</li>
+                    <li><strong>Downloads: </strong>{image.downloads}</li>
+                    <li><strong>Likes: </strong>{image.likes}</li>
                 </ul>
             </div>
             <div className="px-6 py-4">
