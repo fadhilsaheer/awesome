990a9488c5408edcbcd129ac504d5cda8810e71c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-13 14:59:42 +0530

    created serach component ui

---
+++ awesome-js/awesome-react/image_gallery/src/components/ImageSearch.js
@@ -0,0 +1,20 @@
+function ImageSearch({ setTerm }) {
+    return (
+        <div className="max-w-sm rounded overflow-hidden my-10 max-auto">
+            <form className="w-full max-w-sm">
+                <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+                    <input 
+                        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
+                        type="text"
+                        placeholder="Search Image Term.."
+                    />
+                    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default ImageSearch;
