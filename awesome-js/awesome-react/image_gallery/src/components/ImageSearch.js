20df6be460e9491dff2905c73d4893b8618f1e17
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-13 15:15:30 +0530

    updated teal with green

--- awesome-js/awesome-react/image_gallery/src/components/ImageSearch.js
+++ awesome-js/awesome-react/image_gallery/src/components/ImageSearch.js
@@ -11,7 +11,7 @@ function ImageSearch({ searchText }) {
     return (
         <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
             <form className="w-full max-w-sm" onSubmit={onSubmit}>
-                <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+                <div className="flex items-center border-b border-b-2 border-green-500 py-2">
                     <input 
                         className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
                         type="text" 
@@ -19,7 +19,7 @@ function ImageSearch({ searchText }) {
                         value={text}
                         onChange={(e)=> setText(e.target.value)}
                     />
-                    <button className="flex-shrink-0 bg-teal hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                    <button className="flex-shrink-0 bg-green-400 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                         Search
                     </button>
                 </div>
