9e68f6321ee2d881b670996f5d995b7313f37f2f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-13 15:10:50 +0530

    completed image search component

--- awesome-js/awesome-react/image_gallery/src/components/ImageSearch.js
+++ awesome-js/awesome-react/image_gallery/src/components/ImageSearch.js
@@ -1,14 +1,25 @@
-function ImageSearch({ setTerm }) {
+import { useState } from 'react';
+
+function ImageSearch({ searchText }) {
+    const [text, setText] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        searchText(text);
+    }
+
     return (
-        <div className="max-w-sm rounded overflow-hidden my-10 max-auto">
-            <form className="w-full max-w-sm">
+        <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+            <form className="w-full max-w-sm" onSubmit={onSubmit}>
                 <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
                     <input 
                         className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
-                        type="text"
-                        placeholder="Search Image Term.."
+                        type="text" 
+                        placeholder="Search Image Term..."
+                        value={text}
+                        onChange={(e)=> setText(e.target.value)}
                     />
-                    <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
+                    <button className="flex-shrink-0 bg-teal hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                         Search
                     </button>
                 </div>
