0e7a5a02a005e8c03d60b0e3ddad6e962bea7bdc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-13 14:41:37 +0530

    looped through images

--- awesome-js/awesome-react/image_gallery/src/App.js
+++ awesome-js/awesome-react/image_gallery/src/App.js
@@ -1,4 +1,5 @@
 import React,{ useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
 import API_KEY from './env';
 
 const apiKey = process.env.apiKey || API_KEY;
@@ -18,32 +19,16 @@ function App() {
             setImages(data.hits);
             setIsLoading(false);
         })
-
-    }, []);
+    }, [term]);
 
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img src="https://source.unsplash.com/random" className="w-full" alt="awesome" />
-            <div className="px-6 py-4">
-                <div className="font-bold text-purple-500 text-xl mb-2">Photo by awesome</div>
-                <ul>
-                    <li><strong>Views: </strong>4000</li>
-                    <li><strong>Downloads: </strong>300</li>
-                    <li><strong>Likes: </strong>400</li>
-                </ul>
-            </div>
-            <div className="px-6 py-4">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                    #hashtag
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                    #hashtag
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                    #hashtag
-                </span>
-            </div>
-        </div>
+       <div className="container mx-auto">
+           <div className="grid grid-cols-3 gap-4">
+                {images && images.map(image => (
+                    <ImageCard key={image.id} image={image} />
+                ))}
+           </div>
+       </div> 
     );
 }
 
