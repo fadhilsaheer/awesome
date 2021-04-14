e6ee0baa10f6db80f6298293df459c74fbf45bdc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 17:32:30 +0530

    created subscription

--- awesome-js/awesome-react/chat-o-matic/server/server.js
+++ awesome-js/awesome-react/chat-o-matic/server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require("graphql-yoga");
+const { GraphQLServer, PubSub } = require("graphql-yoga");
 
 const messages = [];
 
@@ -16,7 +16,14 @@ const typeDefs = `
     type Mutation {
         postMessage(user: String!, content: String!): ID!
     }
+
+    type Subscription{
+        messages: [Message!]
+    }
 `
+
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
 
 const resolvers = {
     Query: {
@@ -26,11 +33,23 @@ const resolvers = {
         postMessage: (parent, { user, content }) => {
             const id = messages.length;
             messages.push({ id, user, content });
+            subscribers.forEach(fn => fn());
             return id;
+        }
+    },
+    Subscription: {
+        messages: {
+            subscribe: (parent, args, { pubsub }) => {
+                const channel = Math.random().toString(36).slice(2, 15);
+                onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+                setTimeout(() => pubsub.publish(channel, { messages }), 0);
+                return pubsub.asyncIterator(channel);
+            }
         }
     }
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 // server.start(({ port }) => console.log(`server started at port http://localhost:${port}/`));
 server.start({ port: 8000 }, ({ port }) => console.log(`server started at port http://localhost:${port}/`));
