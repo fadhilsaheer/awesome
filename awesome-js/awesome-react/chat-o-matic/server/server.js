f555032bb4d6c6589c6150816b3f010f3508d37b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 10:07:29 +0530

    returned id

--- awesome-js/awesome-react/chat-o-matic/server/server.js
+++ awesome-js/awesome-react/chat-o-matic/server/server.js
@@ -26,6 +26,7 @@ const resolvers = {
         postMessage: (parent, { user, content }) => {
             const id = messages.length;
             messages.push({ id, user, content });
+            return id;
         }
     }
 }
