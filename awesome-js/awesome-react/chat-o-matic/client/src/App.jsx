2e92455d8dab9de55c528a73b1392d87b53d82aa
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 10:36:04 +0530

    basic app setup

--- awesome-js/awesome-react/chat-o-matic/client/src/App.jsx
+++ awesome-js/awesome-react/chat-o-matic/client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "shards-ui/dist/css/shards.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 
 import "./index.css";
+import Chat from "./Chat";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <Chat />;
 
 ReactDOM.render(<App />, document.getElementById("app"));
