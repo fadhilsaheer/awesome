d90198eff90e68e90d6a9e0edd3979658dfc8beb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 10:05:30 +0530

    created mutation for post messge

--- awesome-js/awesome-react/chat-o-matic/server/server.js
+++ awesome-js/awesome-react/chat-o-matic/server/server.js
@@ -12,11 +12,21 @@ const typeDefs = `
     type Query {
         messages: [Message!]
     }
+
+    type Mutation {
+        postMessage(user: String!, content: String!): ID!
+    }
 `
 
 const resolvers = {
     Query: {
         messages: () => messages,
+    },
+    Mutation: {
+        postMessage: (parent, { user, content }) => {
+            const id = messages.length;
+            messages.push({ id, user, content });
+        }
     }
 }
 
