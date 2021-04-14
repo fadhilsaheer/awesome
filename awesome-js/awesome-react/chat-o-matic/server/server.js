60b2492f8cf1b7c0e53d143c2041f9ccaf8a7184
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 09:54:03 +0530

    type defs

--- awesome-js/awesome-react/chat-o-matic/server/server.js
+++ awesome-js/awesome-react/chat-o-matic/server/server.js
@@ -1,6 +1,16 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+const typedefs = `
+    type Message {
+        id: ID!
+        user: String!,
+        content: String!
+    }
 
+    type Query {
+        messages: [Messages!]
+    }
+`
 
-const server = new GraphQLServer();
+const server = new GraphQLServer({ typeDefs });
 server.start(({ port }) => console.log(`server started at port http://localhost:${port}/`));
