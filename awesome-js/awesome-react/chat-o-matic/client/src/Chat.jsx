7a676484f71d42b5bb4024348592b6048f0a9fbd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 10:57:30 +0530

    create profile for other user message

--- awesome-js/awesome-react/chat-o-matic/client/src/Chat.jsx
+++ awesome-js/awesome-react/chat-o-matic/client/src/Chat.jsx
@@ -37,6 +37,22 @@ const Messages = ({ user }) => {
                             paddingBottom: '1em',
                         }}
                     >
+                        {!isMe && (
+                            <div
+                                style={{
+                                    height: 50,
+                                    width: 50,
+                                    marginRight: '0.5em',
+                                    border: "2px solid #e5e6ea",
+                                    borderRadius: 25,
+                                    textAlign: "center",
+                                    fontSize: "18pt",
+                                    paddingTop: 5,
+                                }}
+                            >
+                                {messageUser.slice(0, 2).toUpperCase()}
+                            </div>
+                        )}
                         <div
                             style={{
                                 background: isMe ? '#58bf56' : '#e5e6ea',
