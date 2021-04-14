dcf7053e71fe42f1e86eb4285f99e77ae5820d65
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-14 10:26:21 +0530

    ...

--- awesome-js/awesome-react/chat-o-matic/client/src/App.jsx
+++ awesome-js/awesome-react/chat-o-matic/client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "shards-ui/dist/css/shards.min.css"
+
 import "./index.css";
 
 const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
