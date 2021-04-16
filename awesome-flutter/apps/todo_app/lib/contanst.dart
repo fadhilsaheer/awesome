bec79f73ccf6676b0816c1cd55305e891ace77ec
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-16 17:42:19 +0530

    deleted todo app project

--- awesome-flutter/apps/todo_app/lib/contanst.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Color primaryColor = Color(0xff212730);
-// Color secondaryColor = Colors.blueGrey[900];
-const Color primaryColor = Color(0xFF252A36);
-const Color secondayrColor = Color(0xFF333848);
-Color accentColor = Color(0xff1abc9c);
