3cd1afd2cc8f39d51dbf9e5504cb7f5f0501f2a5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-19 17:30:52 +0530

    changed statefull widget to state less

--- awesome-flutter/production-apps/todoey/lib/screens/home_screen.dart
+++ awesome-flutter/production-apps/todoey/lib/screens/home_screen.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/utils/constanst.dart';
 
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
