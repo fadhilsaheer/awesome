90f41e772c7cf8c22b56457215d49aa565635e94
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-19 15:41:25 +0530

    created functions for statements

--- awesome-js/awesome-react/quizapp/src/App.tsx
+++ awesome-js/awesome-react/quizapp/src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 function App() {
+
+    const startTrivia = async () => {
+
+    }
+
+    const checkAnswer = async (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    const nextQuestion = () => {
+
+    }
+
     return (
         <div className="app">
             <h2>Quizz</h2>
