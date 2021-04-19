2ec71c3c14332f2f1a95f2859c98af4bdffdbca9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-19 17:29:45 +0530

    floating action button and bg color

--- awesome-flutter/production-apps/todoey/lib/screens/home_screen.dart
+++ awesome-flutter/production-apps/todoey/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/utils/constanst.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: appGreen,
+        child: Icon(Icons.add, size: 30.0),
+        onPressed: () {},
+      ),
+      body: SingleChildScrollView(),
+    );
   }
 }
