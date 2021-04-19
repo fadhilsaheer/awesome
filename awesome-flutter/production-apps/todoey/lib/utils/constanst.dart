f34e62e3a3b73ee6f5e22edbc2cf8b20a8e4df5a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-19 17:27:52 +0530

    constants with colors

---
+++ awesome-flutter/production-apps/todoey/lib/utils/constanst.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appPrimaryColor = Color(0xFF252A36);
+const Color appSecondayrColor = Color(0xFF333848);
+const Color appGreen = Color(0xFF1ABC9C);
+const Color appWhite = Color(0xFFFFFDFD);
