cd16d6220f2b59618efd6bbad87e3706ddf96efa
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 17:45:58 +0530

    mapping

--- awesome-flutter/production-apps/todoey/lib/utils/todo_modal.dart
+++ awesome-flutter/production-apps/todoey/lib/utils/todo_modal.dart
@@ -1,7 +1,21 @@
 class Todo {
   String title;
-  bool isResolved;
+  bool done;
   int id;
 
-  Todo({this.title, this.isResolved});
+  Todo({this.title, this.done});
+
+  convertToDb() {
+    var mapping = Map<String, dynamic>();
+
+    mapping['id'] = id;
+    mapping['title'] = title;
+    mapping['done'] = done ? 1 : 0;
+  }
+
+  convertFromDb(data) {
+    Todo newData =
+        Todo(title: data['title'], done: data['done'] == 1 ? true : false);
+    newData.id = data['id'];
+  }
 }
