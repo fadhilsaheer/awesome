1b9d2d8a8d6b0a759d6d10cfbcd95cbb182a7653
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 15:05:33 +0530

    resovle task

--- awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
+++ awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
@@ -1,29 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/utils/constanst.dart';
 
-class TodoContainer extends StatelessWidget {
+class TodoContainer extends StatefulWidget {
   final String title;
   final bool isResolved;
-  final Function onTap;
 
   TodoContainer({this.isResolved, this.title});
 
   @override
+  _TodoContainerState createState() => _TodoContainerState();
+}
+
+class _TodoContainerState extends State<TodoContainer> {
+  String _title;
+  bool _isResolved;
+
+  @override
+  void initState() {
+    super.initState();
+    _isResolved = widget.isResolved;
+    _title = widget.title;
+  }
+
+  void resolveTask() {
+    setState(() {
+      _isResolved = !_isResolved;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
-      margin: EdgeInsets.only(top: 15.0),
-      decoration: BoxDecoration(
-        color: isResolved ? appGreen : appSecondayrColor,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Text(
-        title,
-        style: TextStyle(
-          color: appWhite,
-          fontSize: 20.0,
-          fontWeight: FontWeight.w400,
+    return GestureDetector(
+      onTap: resolveTask,
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+        margin: EdgeInsets.only(top: 15.0),
+        decoration: BoxDecoration(
+          color: _isResolved ? appGreen : appSecondayrColor,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Text(
+          _title,
+          style: TextStyle(
+            color: appWhite,
+            fontSize: 20.0,
+            fontWeight: FontWeight.w400,
+          ),
         ),
       ),
     );
--- awesome-flutter/production-apps/todoey/lib/widgets/todo_list.dart
+++ awesome-flutter/production-apps/todoey/lib/widgets/todo_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:todoey/utils/todo_modal.dart';
 import 'package:todoey/widgets/todo_container.dart';
 
 class TodoList extends StatefulWidget {
@@ -7,21 +9,47 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
+  List<Todo> _list = [
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+    Todo(title: "Create an app", isResolved: false),
+  ];
+
+  List<Widget> _widgetList = List<Widget>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (int index = 0; index != _list.length; index++) {
+      Todo todo = _list[index];
+      _widgetList.add(TodoContainer(
+        title: todo.title,
+        isResolved: todo.isResolved,
+      ));
+    }
+  }
+
+  void resolveTask(index) {
+    setState(() {
+      _list[index].isResolved = !_list[index].isResolved;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 20.0),
       child: Column(
-        children: [
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-          TodoContainer(title: "Create an app", isResolved: false),
-        ],
+        children: _widgetList,
       ),
     );
   }
