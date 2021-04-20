7ff9f444536d3917a1cf0be312ce1d8cebfc7989
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 15:28:53 +0530

    created actions

--- awesome-flutter/production-apps/todoey/lib/widgets/alert_form.dart
+++ awesome-flutter/production-apps/todoey/lib/widgets/alert_form.dart
@@ -2,11 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:todoey/utils/constanst.dart';
 
 class AlertForm extends StatelessWidget {
+  String todoTitle;
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: appSecondayrColor,
-      title: Text("New Todo"),
+      title: Text(
+        "New Todo",
+        style: TextStyle(color: appWhite),
+      ),
+      content: TextField(
+        style: TextStyle(color: appWhite),
+        cursorColor: appGreen,
+        onChanged: (String value) => todoTitle = value,
+        decoration: InputDecoration(
+          labelText: "I wanna do ...",
+          labelStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: appGreen, width: 1.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: appGreen, width: 1.0),
+          ),
+        ),
+      ),
+      actions: [
+        FlatButton(
+          child: Text("cancel"),
+          textColor: Colors.redAccent,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        FlatButton(
+          child: Text("create"),
+          textColor: appGreen,
+          onPressed: () {
+            print(todoTitle);
+          },
+        ),
+      ],
     );
   }
 }
