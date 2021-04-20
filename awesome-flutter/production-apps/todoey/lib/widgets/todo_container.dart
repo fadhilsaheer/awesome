310fd68e3affd45340962bb617b9ba2059cb1209
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 14:42:27 +0530

    converted todo container to statefull widget

--- awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
+++ awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/utils/constanst.dart';
 
-class TodoContainer extends StatelessWidget {
+class TodoContainer extends StatefulWidget {
   final String title;
   final bool isResolved;
 
   TodoContainer({this.isResolved, this.title});
 
+  @override
+  _TodoContainerState createState() => _TodoContainerState();
+}
+
+class _TodoContainerState extends State<TodoContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,11 +19,11 @@ class TodoContainer extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
       margin: EdgeInsets.only(top: 15.0),
       decoration: BoxDecoration(
-        color: isResolved ? appGreen : appSecondayrColor,
+        color: widget.isResolved ? appGreen : appSecondayrColor,
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Text(
-        title,
+        widget.title,
         style: TextStyle(
           color: appWhite,
           fontSize: 20.0,
