e8ac1eecdff7d32c4d5e59e44483fc8734d630b2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 18:08:04 +0530

    forget to return

--- awesome-flutter/production-apps/todoey/lib/utils/todo_modal.dart
+++ awesome-flutter/production-apps/todoey/lib/utils/todo_modal.dart
@@ -11,11 +11,15 @@ class Todo {
     mapping['id'] = id;
     mapping['title'] = title;
     mapping['done'] = done ? 1 : 0;
+
+    return mapping;
   }
 
   convertFromDb(data) {
     Todo newData =
         Todo(title: data['title'], done: data['done'] == 1 ? true : false);
     newData.id = data['id'];
+
+    return newData;
   }
 }
