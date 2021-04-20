a08b109a0a9df9acffb946170b53a24f32d21d6b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 08:33:04 +0530

    styled todo container

--- awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
+++ awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/utils/constanst.dart';
 
 class TodoContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+      margin: EdgeInsets.only(top: 15.0),
+      decoration: BoxDecoration(
+        color: appSecondayrColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Text(
+        "todo item",
+        style: TextStyle(
+          color: appWhite,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
   }
 }
