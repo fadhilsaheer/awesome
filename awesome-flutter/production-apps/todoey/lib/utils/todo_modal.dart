e344cf4ca8729f0e7781b435e23e4efbb4aa6ff4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-20 14:47:41 +0530

    todo modal

---
+++ awesome-flutter/production-apps/todoey/lib/utils/todo_modal.dart
@@ -0,0 +1,5 @@
+class Todo {
+  String title;
+  bool isResolved;
+  int id;
+}
--- awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
+++ awesome-flutter/production-apps/todoey/lib/widgets/todo_container.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/utils/constanst.dart';
 
-class TodoContainer extends StatefulWidget {
+class TodoContainer extends StatelessWidget {
   final String title;
   final bool isResolved;
+  final Function onTap;
 
   TodoContainer({this.isResolved, this.title});
 
-  @override
-  _TodoContainerState createState() => _TodoContainerState();
-}
-
-class _TodoContainerState extends State<TodoContainer> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,11 +15,11 @@ class _TodoContainerState extends State<TodoContainer> {
       padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
       margin: EdgeInsets.only(top: 15.0),
       decoration: BoxDecoration(
-        color: widget.isResolved ? appGreen : appSecondayrColor,
+        color: isResolved ? appGreen : appSecondayrColor,
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Text(
-        widget.title,
+        title,
         style: TextStyle(
           color: appWhite,
           fontSize: 20.0,
