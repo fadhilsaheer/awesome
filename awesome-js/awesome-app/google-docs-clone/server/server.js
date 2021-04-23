590bb68560a9364d30c9f82d002c3cdc8d86a86c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-23 23:57:35 +0530

    socket server

--- awesome-js/awesome-app/google-docs-clone/server/server.js
+++ awesome-js/awesome-app/google-docs-clone/server/server.js
@@ -1,3 +1,8 @@
 const io = require("socket.io")(5000, {
-    
+    cors: {
+        origin: 'http://localhost:4122', // change it accordingly
+        methods: ['GET', 'POST']
+    }
 });
+
+io.on('connection', socket => {});
