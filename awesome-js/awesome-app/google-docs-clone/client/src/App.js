fc5ef3e2c46dfa1f890241c1992fe19d628021f5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-24 07:09:00 +0530

    created router

--- awesome-js/awesome-app/google-docs-clone/client/src/App.js
+++ awesome-js/awesome-app/google-docs-clone/client/src/App.js
@@ -1,7 +1,20 @@
 import TextEditor from './TextEditor';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
 
 function App() {
-    return <TextEditor />;
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <Redirect to={`/documents/${uuidV4()}`} />
+                </Route>
+                <Route path='/documents/:id'>
+                    <TextEditor />;
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
