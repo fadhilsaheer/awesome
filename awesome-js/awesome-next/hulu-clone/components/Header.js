6dfe2a14738b39b6300408d4ba622a1b80024dcd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-25 09:18:50 +0530

    used header item

--- awesome-js/awesome-next/hulu-clone/components/Header.js
+++ awesome-js/awesome-next/hulu-clone/components/Header.js
@@ -1,8 +1,26 @@
 import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from '@heroicons/react/outline';
+
 
 function Header() {
     return (
         <header className=''>
+            <div>
+                <HeaderItem title='HOME' Icon={HomeIcon} />
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+                <HeaderItem title='COLLECTION' Icon={CollectionIcon} />
+                <HeaderItem title='SEARCH' Icon={SearchIcon} />
+                <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+            </div>
             <Image
                 src="https://links.papareact.com/ua6"
                 width={200}
