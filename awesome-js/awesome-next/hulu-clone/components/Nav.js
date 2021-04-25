f552e834169af88914211209474c0ec122bbf159
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-25 09:26:58 +0530

    created nav component

---
+++ awesome-js/awesome-next/hulu-clone/components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Nav
--- awesome-js/awesome-next/hulu-clone/pages/index.js
+++ awesome-js/awesome-next/hulu-clone/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             </Head>
 
             <Header />
+            <Nav />
 
         </div>
     )
