e1ba0c42d9118c492d338c9541a47184511ee500
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-25 10:05:57 +0530

    fade effect and styles

--- awesome-js/awesome-next/hulu-clone/components/Nav.js
+++ awesome-js/awesome-next/hulu-clone/components/Nav.js
@@ -1,13 +1,21 @@
 import requests from '../utils/requests';
+import { useRouter } from 'next/router';
 
 function Nav() {
+    const router = useRouter();
+
     return (
-        <nav>
-            <div className="">
-                {Object.entries(requests).map(([key, { title, urls }]) => (
-                    <h2 key={key}>{title}</h2>
+        <nav className='relative'>
+            <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 overflow-x-scroll scrollbar-hide">
+                {Object.entries(requests).map(([key, { title, url }]) => (
+                    <h2
+                        className='last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red'
+                        key={key}
+                        onClick={() => router.push(`/?genre=${key}`)}
+                    >{title}</h2>
                 ))}
             </div>
+            <div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12' />
         </nav>
     )
 }
