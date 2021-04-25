bd00c59a5ea53e7b2fd9fc8c224dc41e00fe8116
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-25 08:57:11 +0530

    added logo

--- awesome-js/awesome-next/hulu-clone/components/Header.js
+++ awesome-js/awesome-next/hulu-clone/components/Header.js
@@ -1,7 +1,14 @@
+import Image from 'next/image';
+
 function Header() {
     return (
         <header className=''>
-            <h1>This is a header</h1>
+            <Image
+                src="https://links.papareact.com/ua6"
+                width={200}
+                height={100}
+                className="object-container"
+            />
         </header>
     )
 }
