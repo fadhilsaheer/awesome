128c2cbdb730dc0064d44ed9bd1358c57dee8f98
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-27 06:13:01 +0530

    setuped componentn for result

--- awesome-js/awesome-next/hulu-clone/pages/index.js
+++ awesome-js/awesome-next/hulu-clone/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
 
             <Header />
             <Nav />
+            <Results />
 
         </div>
     )
