634ae00e0345418b524a3dc484f40b64e10851a7
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-27 06:33:08 +0530

    fetching data

--- awesome-js/awesome-next/hulu-clone/pages/index.js
+++ awesome-js/awesome-next/hulu-clone/pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({ results }) {
     return (
         <div>
             <Head>
@@ -13,8 +14,17 @@ export default function Home() {
 
             <Header />
             <Nav />
-            <Results />
+            <Results results={results} />
 
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    const genre = context.query.genre;
+
+    const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then(res => res.json());
+
+    return { props: { results: request.results } }
+
 }
