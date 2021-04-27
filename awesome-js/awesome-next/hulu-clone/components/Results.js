04d7c1fe465fd62309c53fcb48aa377eacef6a47
fadhilsaheer <fadhilsaheer@gmail.com>
2021-04-27 06:35:55 +0530

    mapped through results

--- awesome-js/awesome-next/hulu-clone/components/Results.js
+++ awesome-js/awesome-next/hulu-clone/components/Results.js
@@ -1,7 +1,11 @@
-function Results() {
+import Thumbnail from "./Thumbnail"
+
+function Results({ results }) {
     return (
         <div>
-
+            {results.map(result => (
+                <Thumbnail key={result.id} result={result} />
+            ))}
         </div>
     )
 }
