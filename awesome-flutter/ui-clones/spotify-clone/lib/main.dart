b6ba67d60e440f76f7ae927ad5caec24c2468f40
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-07 09:12:34 +0530

    widget defualt export

--- awesome-flutter/ui-clones/spotify-clone/lib/main.dart
+++ awesome-flutter/ui-clones/spotify-clone/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/widgets/widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -64,11 +65,7 @@ class Shell extends StatelessWidget {
           Expanded(
             child: Row(
               children: [
-                Container(
-                  width: 280.0,
-                  color: Colors.green,
-                  height: double.infinity,
-                ),
+                SideMenu(),
                 // plylist
               ],
             ),
