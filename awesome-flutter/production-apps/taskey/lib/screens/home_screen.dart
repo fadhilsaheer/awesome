377a29a0656b32f390c8a8d5d9edaf1b37eee27a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-07 15:29:57 +0530

    basic home screen

--- awesome-flutter/production-apps/taskey/lib/screens/home_screen.dart
+++ awesome-flutter/production-apps/taskey/lib/screens/home_screen.dart
@@ -5,19 +5,39 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: appPrimaryColor,
-      body: ListView(
-        children: [
-          Text(
-            "Taskey",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 25.0,
-              fontFamily: 'font',
+        backgroundColor: appPrimaryColor,
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Taskey",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60.0,
+                    fontFamily: 'font',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "Take notes, reminders, saet targets,\ncollect resources, and secure privacy",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20.0,
+                    fontFamily: 'nunito',
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text("Get Started"),
+                  color: appPrimaryAccent,
+                )
+              ],
             ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
