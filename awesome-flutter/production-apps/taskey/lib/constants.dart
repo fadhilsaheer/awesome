28a0f1c392d4b6e51e1634bc8b57c06a241bc579
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-07 15:00:18 +0530

    constants file with colors

---
+++ awesome-flutter/production-apps/taskey/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appPrimaryColor = Color(0xFF1F1D2B);
+const Color appSecondaryColor = Color(0xFF272636);
+const Color appPrimaryAccent = Color(0xFF6F6FC8);
+const Color appSecondaryAccent = Color(0xFFFFC04A);
