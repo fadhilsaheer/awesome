d0107402982c9acafba3b10a78c656e802ceb8e2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-07 09:16:28 +0530

    added logo

--- awesome-flutter/ui-clones/spotify-clone/lib/widgets/side_menu.dart
+++ awesome-flutter/ui-clones/spotify-clone/lib/widgets/side_menu.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 class SideMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: double.infinity,
+      width: 280.0,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'assets/spotify_logo.png',
+                  height: 55.0,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
