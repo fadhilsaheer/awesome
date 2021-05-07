2b8625114de9bfd37c9e9def8863bda58aab5a6d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-07 09:09:54 +0530

    platform dependent

--- awesome-flutter/ui-clones/spotify-clone/lib/main.dart
+++ awesome-flutter/ui-clones/spotify-clone/lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && (Platform.isMacOS || Platform.isLinux || Platform.isWindows)) {
+    await DesktopWindow.setMinWindowSize(const Size(600, 800));
+  }
   runApp(MyApp());
 }
 
