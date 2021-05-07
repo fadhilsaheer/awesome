8a9fd69b979ab9b77056de768d781191cab524eb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-07 15:13:48 +0530

    used font family

--- awesome-flutter/production-apps/taskey/lib/screens/home_screen.dart
+++ awesome-flutter/production-apps/taskey/lib/screens/home_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:taskey/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      body: ListView(
+        children: [
+          Text(
+            "Taskey",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+              fontFamily: 'font',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
