1232803f53244b5f1e3e96a75d0f888dc42ef5b4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-08 13:37:54 +0530

    deleted taskey project

--- awesome-flutter/production-apps/taskey/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// colors
-
-const Color appPrimaryColor = Color(0xFF1F1D2B);
-const Color appSecondaryColor = Color(0xFF272636);
-const Color appPrimaryAccent = Color(0xFF6F6FC8);
-const Color appSecondaryAccent = Color(0xFFFFC04A);
